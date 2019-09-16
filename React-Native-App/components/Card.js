1008fd42f9e9e96ac2cf9e569c52401db728f6c0
Andy Herrera <aherr127@fiu.edu>
2019-09-16 12:34:21 -0400

    added needed images

---
+++ React-Native-App/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Card = () => {
+    return (
+        <View>
+            <Text>Card</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+
+    },
+});
+
+export default Card;
