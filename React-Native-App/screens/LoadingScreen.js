2496f7ef536c348cdd6407d1c9d8c18c5fc58528
Andy Herrera <aherr127@fiu.edu>
2019-09-21 18:30:43 -0400

    fixed loading screen

--- React-Native-App/screens/LoadingScreen.js
+++ React-Native-App/screens/LoadingScreen.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { View, ImageBackground, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
-import Colors from '../constants/Colors';
 
 const Loading = props => {
     return (
 
         <View style={styles.container} onStartShouldSetResponder={() => props.onTap()}>
-            <ImageBackground  source={require('../assets/mom-welcome.png')} style={styles.picture} imageStyle={{opacity:0.2}}>
-                    <Text style={styles.letters}>Keeping Moms</Text>
-                    <Text style={styles.lettersAnd}>&</Text>
-                    <View style={styles.seperator}/>
-                    <Text style={styles.letters}>Infants Healthy</Text>
-            </ImageBackground>
+            <Image source={require('../assets/Title-Icon.png')} style={styles.picture}/>
         </View>
 
     );
@@ -23,27 +17,15 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop:10,
-        marginBottom:10,
+        paddingLeft: 20
     },
     picture: {
-        width: "100%",
-        height: "100%",
-    },
-    letters: {
-        color: Colors.OrangeColor,
-        fontSize: 58,
-        alignSelf: 'center'
-    },
-    lettersAnd: {
-        color: Colors.OrangeColor,
-        fontSize: 58,
-        alignSelf: 'flex-end',
-        padding: 10
-    },
-    seperator: {
-        width: '100%',
-        height: '70%',
+        position: 'absolute',
+        width: 350,
+        height: 350,
+        resizeMode:'contain',
+        justifyContent: 'center', 
+        alignItems: 'center',
     },
 });
 
