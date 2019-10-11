d4e06bcabaf29e8cfbd5aa2eed9a30253749842b
Arelys Alvarez <aalva456@fiu.edu>
2019-10-10 20:54:27 -0400

    create user, request code and verify code

--- React-Native-App/functions/index.js
+++ React-Native-App/functions/index.js
@@ -1,8 +1,15 @@
+const admin = require('firebase-admin'); //to get access to all of he project data in firebase
 const functions = require('firebase-functions');
+const serviceAccount = require("./service_account.json");
+const createUsers = require('./create_user');
+const requestOneTimePassword = require("./request_one_time_password");
+const verifyOneTimePassword = require("./verify_one_time_password");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://moms-and-infants-healthy.firebaseio.com"
 });
+
+exports.createUsers = functions.https.onRequest(createUsers);
+exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
+exports.verifyOneTimePassword = functions.https.onRequest(verifyOneTimePassword);
