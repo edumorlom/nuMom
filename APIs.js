b91f5ddf74e60b3f0956f15b71673bb6d4466f1a
Eduardo Morales <eduardomorales@Eduardos-MacBook-Pro.local>
2020-01-15 13:51:25 -0500

    initial guidelines for #4

---
+++ APIs.js
@@ -0,0 +1,25 @@
+function logIn(email, passsword){
+	
+	
+	
+}
+
+
+
+
+function signUp(name, email, username, password, phoneNumber, address) {
+	//Make sure address is of type json with zipcode, etc.
+
+	
+
+
+}
+
+
+//Testcase for signUp -- Edit here
+signUp(name="", email="", username="", password="", phoneNumber="", address="")
+
+//Testcase for logIn
+//If user doesn't exist, it should not work.
+//If user exists, login.
+login("emora113@fiu.edu", "12345")
