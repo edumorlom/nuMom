632670a340dc760adb929a6b783cbd51df0a2d8f
Eduardo Morales <AzPD5SUKpqVt>
2020-03-21 19:48:45 -0400

    #1 adds portuguese

--- moms-infants-healthy-redesign/getLocalizedText.js
+++ moms-infants-healthy-redesign/getLocalizedText.js
@@ -1,9 +1,11 @@
 import en_US from './en_US'
 import es_ES from './es_ES'
+import pt_BR from './pt_BR'
 
 
 export default function getLocalizedText(deviceLanguage, key) {
     if (!deviceLanguage) return en_US[key];
     if (deviceLanguage.includes("es")) return es_ES[key];
+    if (deviceLanguage.includes("pt")) return pt_BR[key];
     else return en_US[key];
 }
