b08b6f1de5a3fc9265be001cf3b8c2da4f642495
amin sheikhnia <ashei012@fiu.edu>
2020-05-29 16:47:40 -0400

    modified Resources and added an image to assets for STDs

--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -1,22 +1,28 @@
 export default function Clinics() {
-    return (
-        [{
-            "resource": "Breast Feeding Miami",
-            "subtitle": "Learn the amazing process of breast feeding your child",
-            "website": "www.edumorales.dev/",
-            "icon": require("../../assets/breastfeeding.png")
-        },{
-            "resource": "Love Your Child",
-            "subtitle": "Learn to love your kid forever no matter what",
-            "website": "www.edumorales.dev/",
-            "icon": require("../../assets/heart.png")
-        }, {
-            "resource": "Be Your Kid's Psychologist",
-            "subtitle": "Learn to be there for your kid, mentally",
-            "website": "www.edumorales.dev/",
-            "icon": require("../../assets/nature.png")
-        }]
-    )
-
-
+  return [
+    {
+      resource: "Breast Feeding Miami",
+      subtitle: "Learn the amazing process of breast feeding your child",
+      website: "www.edumorales.dev/",
+      icon: require("../../assets/breastfeeding.png"),
+    },
+    {
+      resource: "Love Your Child",
+      subtitle: "Learn to love your kid forever no matter what",
+      website: "www.edumorales.dev/",
+      icon: require("../../assets/heart.png"),
+    },
+    {
+      resource: "Be Your Kid's Psychologist",
+      subtitle: "Learn to be there for your kid, mentally",
+      website: "www.edumorales.dev/",
+      icon: require("../../assets/nature.png"),
+    },
+    {
+      resource: "STD Awareness",
+      subtitle: "Learn about STDs and how they affect you ",
+      website: "www.edumorales.dev/",
+      icon: require("../../assets/butterfly.png"),
+    },
+  ];
 }
