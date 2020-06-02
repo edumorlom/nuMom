bad5b186a0a97312864f6246d3991df06e923d07
ShaderLK <aminspects@gmail.com>
2020-06-01 22:40:59 -0400

    found out how to make the buttons work for learn, but the wording is too large and it gives an error when i click on learn

--- src/Components/Learn.jsx
+++ src/Components/Learn.jsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import {ScrollView} from "react-native";
-import SelectionButtonImageOnRight from "./ClinicSelectionButton";
-import clinicLogo from '../../assets/clinic-logo.png'
-import Resources from './Resources'
-import LearnSelectionButton from "./LearnSelectionButton";
+import React from "react";
+import { TouchableHighlight, Text, View } from "react-native";
+import appStyles from "./AppStyles";
+import SelectionButton from "./SelectionButton";
+import GestureRecognizer from "react-native-swipe-gestures";
+import breastfeeding from "../../assets/breastfeeding.png";
+import heart from "../../assets/heart.png";
+import nature from "../../assets/nature.png";
+import butterfly from "../../assets/butterfly.png";
 
-
-
-
-export default function Learn(props) {
-
-    let resourceButtons = Resources().map((resource, key) =>
-        <LearnSelectionButton key={key}
-                                     icon={resource.icon}
-                                     onPress={() => {
-                                         props.setClinicToView(clinicLogo);
-                                         props.setLowerPanelContent('clinicInfo');
-                                     }} resource={resource}/>);
-
-    return (
-        <ScrollView contentContainerStyle={{alignItems: 'center', maxWidth: '100%'}}>
-            {resourceButtons}
-        </ScrollView>
-    )
+export default function LowerPanelSelection(props) {
+  return (
+    <GestureRecognizer
+      onSwipeUp={() => props.setFullPanel(true)}
+      onSwipeDown={() => props.setFullPanel(false)}
+      config={{ velocityThreshold: 0.4, directionalOffsetThreshold: 100 }}
+      style={{ width: "100%", height: "100%", alignItems: "center" }}
+    >
+      <SelectionButton
+        text={props.getLocalizedText("breastFeedingMiami")}
+        icon={breastfeeding}
+        onPress={() => props.setLowerPanelContent("breastFeedingMiami")}
+      />
+      <SelectionButton
+        text={props.getLocalizedText("loveYourChild")}
+        icon={heart}
+        onPress={() => {
+          props.setLowerPanelContent("loveYourChild");
+        }}
+      />
+      <SelectionButton
+        text={props.getLocalizedText("kidPsychologist")}
+        icon={nature}
+        onPress={() => {
+          props.setLowerPanelContent("kidPsychologist");
+        }}
+      />
+      <SelectionButton
+        text={props.getLocalizedText("STDAwareness")}
+        icon={butterfly}
+        onPress={() => {
+          props.setLowerPanelContent("STDInfo");
+        }}
+      />
+    </GestureRecognizer>
+  );
 }
