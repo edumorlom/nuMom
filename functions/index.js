8dafe18a433fa38bf5ed87791093fe40008a84c9
Gabriel Alfonso <gabrielalfonso99@gmail.com>
2020-06-03 02:53:46 -0400

    Got the weekly Messages working

--- functions/index.js
+++ functions/index.js
@@ -3,27 +3,17 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
-const twilio = require('twilio');
-const twilioAccount = require('./twilio_account');
-const accountSid = twilioAccount.accountSid;
-const authToken = twilioAccount.authToken;
+const getLocalizedText = require('./getLocalizedText');
+const message = require('./message');
 
-const client = new twilio(accountSid, authToken);
-const twilioNumber = '+19046472206';
 
 //To call weeks dynamically from json use require(en_En.json) then do json[week] where week can be "week1" for example
 
 exports.sendCustomSMS = functions.https.onRequest((req, res) => {
- console.log(req.body.message);
- client.messages
-     .create({
-      body: req.body.message,
-      from: twilioNumber,
-      to: req.query.phoneNumber
-     })
-     .then(message => res.status(200).send(message))
-     .catch(e => console.log(e))
-}); 
+  message.sendCustomSMS(req.query.phoneNumber, req.body.message)
+  .then(message =>  res.status(200).send(message))
+  .catch(e => console.log(e))
+});
 
 exports.sendWeeklySMS = functions.https.onRequest((req, res) => {
   var ref = admin.database().ref("users");
@@ -35,37 +25,47 @@ exports.sendWeeklySMS = functions.https.onRequest((req, res) => {
       let phoneNumber = childSnapshot.val().phoneNumber;
       let week = "Week" + childSnapshot.val().week;
       let deviceLanguage = childSnapshot.val().deviceLanguage;
-
       //get messages based on week and device language and whether it is baby message or mother message like "Week1Mother" or "Week1Baby"
 
+      let motherKey = week + "Mother";
+      let babyKey = week + "Baby";
+      let motherMsg = getLocalizedText.translate(deviceLanguage, motherKey)
+      let babyMsg = getLocalizedText.translate(deviceLanguage, babyKey)
+
       //create SMS with message for mother and another with message for baby
+     
+      message.sendCustomSMS(phoneNumber, motherMsg);
+      message.sendCustomSMS(phoneNumber, babyMsg);
 
       //Update nextWeek by adding 7 days
+      //Update week number   (If weekNumber > 24 set it to null)
       let nextweek = new Date(today.getFullYear(), today.getMonth(), today.getDate()+7);
       let nextWeek = (nextweek.getMonth()+1).toString().padStart(2, "0") +'/'+nextweek.getDate().toString().padStart(2, "0") +'/'+ nextweek.getFullYear()
-      //Update week number   (If weekNumber > 24 set it to null)
+      let nextWeekNo = childSnapshot.val().week + 1;
+      (nextWeekNo < 25) ? null : nextWeekNo = null;  //The messages are only for the first 24 weeks.
+      nextWeekNo ? null : nextWeek = null;       //Set the date to null as well. This will remove it from firebase
       childSnapshot.ref.set({
         nextWeek: nextWeek,
-        week: childSnapshot.val().week + 1
+        week: nextWeekNo
       });
     })
     return null;
-  }).catch(e => console.log(e))
+  }).catch(e => console.log(e)) //Redeploy the function
  
 
-
-
-
-
 });
-/*     anon = () => {
-        var ref = firebase.database().ref("users");
-        let today = null;
-        ref.orderByChild("nextWeek").equalTo(today).on("child_added", function(snapshot) {
-            console.log(snapshot.key);
-          });
-    }     */
 
+/* exports.sendCustomSMS = functions.https.onRequest((req, res) => {
+  console.log(req.body.message);
+  client.messages
+      .create({
+       body: req.body.message,
+       from: twilioNumber,
+       to: req.query.phoneNumber
+      })
+      .then(message => res.status(200).send(message))
+      .catch(e => console.log(e))
+ });  */
 
 /* exports.helloWorld = (req, res) => {
     let message = req.query.message || req.body.message || 'Hello World!';
@@ -73,4 +73,3 @@ exports.sendWeeklySMS = functions.https.onRequest((req, res) => {
   };
    */
 
-//functions.database().ref("users") => {     ///maybe ("/users")
