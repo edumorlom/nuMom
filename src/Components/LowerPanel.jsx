7977e8985e5302c6d615397d0f254d3b823f40a1
ShaderLK <aminspects@gmail.com>
2020-06-04 15:23:22 -0400

    fixed lower panel

--- src/Components/LowerPanel.jsx
+++ src/Components/LowerPanel.jsx
@@ -6,12 +6,8 @@ import FindCare from "./FindCare";
 import ClinicInfo from "./ClinicInfo";
 import LowerPanelHeader from "./LowerPanelHeader";
 import Learn from "./Learn";
-import STDInfo from "./STDInfo";
 import Tips from "./TipsAndTricks";
-<<<<<<< HEAD
-=======
-
->>>>>>> 142cfab641bf7a39d8022710a8a96578cf6ef2bc
+import STDInfo from "./STDInfo";
 
 export default class LowerPanel extends React.Component {
   constructor(props) {
@@ -40,18 +36,18 @@ export default class LowerPanel extends React.Component {
 
   goDown = () => {
     clearInterval(this.transition);
-    this.transition = setInterval( () => {
-        let panelStyle = {...appStyles.lowerPanel};
-        panelStyle["bottom"] = this.state.panelStyle.bottom - 30;
+    this.transition = setInterval(() => {
+      let panelStyle = { ...appStyles.lowerPanel };
+      panelStyle["bottom"] = this.state.panelStyle.bottom - 30;
 
       if (this.state.panelStyle.bottom <= appStyles.lowerPanel.bottom) {
         clearInterval(this.transition);
         panelStyle["bottom"] = appStyles.lowerPanel.bottom;
       }
 
-      /* this.setState({ panelStyle: panelStyle });
+      this.setState({ panelStyle: panelStyle });
     }, 0.1);
-  }; */
+  };
 
   showContent = () => {
     if (this.props.lowerPanelContent === "findCare") {
@@ -101,6 +97,7 @@ export default class LowerPanel extends React.Component {
           fullPanel={this.props.fullPanel}
           setLowerPanelContent={this.props.setLowerPanelContent}
           getLocalizedText={this.props.getLocalizedText}
+          setAppState={this.props.setAppState}
         />
       );
     }
