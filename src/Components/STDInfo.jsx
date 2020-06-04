9de010168bc2cf49b20f8cf5f9299d181c8a223f
ShaderLK <aminspects@gmail.com>
2020-06-04 11:47:05 -0400

    Pre-Info

--- src/Components/STDInfo.jsx
+++ src/Components/STDInfo.jsx
@@ -3,10 +3,6 @@ import { TouchableHighlight, Text, View } from "react-native";
 import appStyles from "./AppStyles";
 import LearnSelectionButton from "./LearnSelectionButton";
 import GestureRecognizer from "react-native-swipe-gestures";
-import breastfeeding from "../../assets/breastfeeding.png";
-import heart from "../../assets/heart.png";
-import nature from "../../assets/nature.png";
-import butterfly from "../../assets/butterfly.png";
 
 export default function STDInfo(props) {
   return (
@@ -16,25 +12,16 @@ export default function STDInfo(props) {
       config={{ velocityThreshold: 0.4, directionalOffsetThreshold: 100 }}
       style={{ width: "100%", height: "100%", alignItems: "center" }}
     >
-      <LearnSelectionButton
-        text={props.getLocalizedText("breastFeedingMiami")}
-        icon={breastfeeding}
-        onPress={() => props.setLowerPanelContent("breastFeedingMiami")}
-      />
-      <LearnSelectionButton
-        text={props.getLocalizedText("loveYourChild")}
-        icon={heart}
-        onPress={() => {
-          props.setLowerPanelContent("loveYourChild");
+      <Text
+        style={{
+          color: props.color,
+          fontSize: appStyles.regularFontSize - 4,
+          fontWeight: "bold",
+          textAlign: "center",
         }}
-      />
-      <LearnSelectionButton
-        text={props.getLocalizedText("kidPsychologist")}
-        icon={nature}
-        onPress={() => {
-          props.setLowerPanelContent("kidPsychologist");
-        }}
-      />
+      >
+        {"To Be Added"}
+      </Text>
     </GestureRecognizer>
   );
 }
