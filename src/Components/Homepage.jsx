b135b237f386fdd3c6ce17073d0ca942031afc6b
ShaderLK <aminspects@gmail.com>
2020-06-04 21:05:29 -0400

    Added the back button

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,90 +1,103 @@
-import React from 'react';
+import React from "react";
 import Map from "./Map";
-import {View} from 'react-native';
+import { View } from "react-native";
 import LowerPanel from "./LowerPanel";
 import SOSButton from "./SOSButton";
 import appStyles from "./AppStyles";
 import Clinics from "./Clinics";
 
-
-
 export default class Homepage extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {fullPanel: true, clinics: Clinics(), clinicToView: null, lowerPanelContent: 'selection', currentLocation: this.getCurrentLocation() ? this.getCurrentLocation() : null};
-        //this.getCurrentLocation();
-    }
-
-
-    
-
-    getCurrentLocation = () => {
-        this.watchID = navigator.geolocation.watchPosition((position) => {
-            let currentLocation = {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-            };
-            
-            return currentLocation;
-            //this.setState({currentLocation: currentLocation})
-        }, (error) => console.log(error));
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullPanel: true,
+      clinics: Clinics(),
+      clinicToView: null,
+      lowerPanelContent: "selection",
+      currentLocation: this.getCurrentLocation()
+        ? this.getCurrentLocation()
+        : null,
     };
+    //this.getCurrentLocation();
+  }
 
-    componentWillUnmount() {
-        navigator.geolocation.clearWatch(this.watchID);
-    }
+  getCurrentLocation = () => {
+    this.watchID = navigator.geolocation.watchPosition(
+      (position) => {
+        let currentLocation = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        };
 
-    setFullPanel = (fullPanel) => {
-        this.state.fullPanel && !fullPanel ? this.setState({fullPanel: fullPanel}) : null;
-        ((!this.state.fullPanel) && fullPanel) ? this.setState({fullPanel: fullPanel}) : null
-    };
+        return currentLocation;
+        //this.setState({currentLocation: currentLocation})
+      },
+      (error) => console.log(error)
+    );
+  };
 
-    setClinicToView = (clinic) => {
-        this.setState({clinicToView: clinic})
-    };
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
 
+  setFullPanel = (fullPanel) => {
+    this.state.fullPanel && !fullPanel
+      ? this.setState({ fullPanel: fullPanel })
+      : null;
+    !this.state.fullPanel && fullPanel
+      ? this.setState({ fullPanel: fullPanel })
+      : null;
+  };
 
-    setLowerPanelContent = (lowerPanelContent) => {
-        this.setState({lowerPanelContent: lowerPanelContent});
-    };
+  setClinicToView = (clinic) => {
+    this.setState({ clinicToView: clinic });
+  };
 
-    goBack = () => {
-        if (this.state.lowerPanelContent === 'selection') return;
-        if (this.state.lowerPanelContent === 'findCare') this.setLowerPanelContent('selection');
-        if (this.state.lowerPanelContent === 'clinicInfo') this.setLowerPanelContent('findCare');
-        if (this.state.lowerPanelContent === 'learn') this.setLowerPanelContent('selection');
-        if (this.state.lowerPanelContent === 'tips') this.setLowerPanelContent('selection');
-        
-    };
+  setLowerPanelContent = (lowerPanelContent) => {
+    this.setState({ lowerPanelContent: lowerPanelContent });
+  };
 
+  goBack = () => {
+    if (this.state.lowerPanelContent === "selection") return;
+    if (this.state.lowerPanelContent === "findCare")
+      this.setLowerPanelContent("selection");
+    if (this.state.lowerPanelContent === "clinicInfo")
+      this.setLowerPanelContent("findCare");
+    if (this.state.lowerPanelContent === "learn")
+      this.setLowerPanelContent("selection");
+    if (this.state.lowerPanelContent === "STDInfo")
+      this.setLowerPanelContent("learn");
+    if (this.state.lowerPanelContent === "tips")
+      this.setLowerPanelContent("selection");
+  };
 
-    render() {
-        return (
-            <View style={appStyles.container}>
-                <Map onPress={() => this.setFullPanel(false)}
-                      setFullPanel={this.setFullPanel}
-                      clinicToView={this.state.clinicToView}
-                      setClinicToView={this.setClinicToView}
-                      clinics={this.state.clinics}
-                      currentLocation={this.state.currentLocation}
-                      getLocalizedText={this.props.getLocalizedText}/>
-                <SOSButton/>
-                <LowerPanel //onPress={() => this.setFullPanel(true)}
-                            setFullPanel={() => this.setFullPanel(!this.state.fullPanel)}
-                            fullPanel={this.state.fullPanel}
-                            fullName={this.props.fullName}
-                            logout={this.props.logout}
-                            clinics={this.state.clinics}
-                            clinicToView={this.state.clinicToView}
-                            setClinicToView={this.setClinicToView}
-                            lowerPanelContent={this.state.lowerPanelContent}
-                            goBack={this.goBack}
-                            setLowerPanelContent={this.setLowerPanelContent}
-                            getLocalizedText={this.props.getLocalizedText}
-                            setAppState={this.props.setAppState}
-                            />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={appStyles.container}>
+        <Map
+          onPress={() => this.setFullPanel(false)}
+          setFullPanel={this.setFullPanel}
+          clinicToView={this.state.clinicToView}
+          setClinicToView={this.setClinicToView}
+          clinics={this.state.clinics}
+          currentLocation={this.state.currentLocation}
+          getLocalizedText={this.props.getLocalizedText}
+        />
+        <SOSButton />
+        <LowerPanel //onPress={() => this.setFullPanel(true)}
+          setFullPanel={() => this.setFullPanel(!this.state.fullPanel)}
+          fullPanel={this.state.fullPanel}
+          fullName={this.props.fullName}
+          logout={this.props.logout}
+          clinics={this.state.clinics}
+          clinicToView={this.state.clinicToView}
+          setClinicToView={this.setClinicToView}
+          lowerPanelContent={this.state.lowerPanelContent}
+          goBack={this.goBack}
+          setLowerPanelContent={this.setLowerPanelContent}
+          getLocalizedText={this.props.getLocalizedText}
+        />
+      </View>
+    );
+  }
 }
