8cc0a1efec847baf72059b19c33b3a581710089d
nvela031 <nvela031@fiu.edu>
2020-06-16 21:42:42 -0400

    Switch the SettingScreen Component from class to function and use UseState and UseEffect hooks

--- src/Components/SettingScreen.jsx
+++ src/Components/SettingScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { useState, useEffect, useRef, Component } from "react";
 import { TextInputMask } from 'react-native-masked-text';
 import {
   View,
@@ -21,52 +21,35 @@ import { AntDesign } from '@expo/vector-icons';
 
 
 
-class SettingScreen extends React.Component {
+const SettingScreen = (props) =>  {
 
-  _isMounted = false;
+ 
 
-  constructor(props){
-    super(props);
-    this.state = { 
-      phoneNumber: null,  
-      dob: null, 
-      pregnant: null, 
-      infant: null, 
-      babyGender:{
-        male: null,
-        female: null
-      }, 
-      liveMiami: null,
-      fullName: null,
-      babyDOB: null,
-      };
+    const [phoneNumber, setPhoneNumber] = useState(null);
+    const [dob, setdob] = useState(null);
+    const [fullName, setFullName] = useState(null);
+    const [liveMiami, setLiveMiami] = useState(null);
+    const [infant, setInfant] = useState(null);
+    // const [babyGender, setBabyGender] = useState({male: null, female: null});
+    const [babyDOB, setBabyDOB] = useState(null);
+    const [pregnant, setPregnant] = useState(null);
+    const datetimeField = useRef(null);
+     let _isMounted = false;
 
-    this.onChangeText = this.onChangeText.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    
-
-  }
-  
-
-
-
-  onChangeText = (object) => {
-    this.setState(object);
-  };
 
    goBack = () => {
     Haptics.selectionAsync().then();
-    this.props.goBack();
+    props.goBack();
   };
 
     AsyncAlert = () => {
     return new Promise((resolve, reject) => {
       Alert.alert(
-        this.props.getLocalizedText("logout"),
-        this.props.getLocalizedText('WantToLogout'),
+        props.getLocalizedText("logout"),
+        props.getLocalizedText('WantToLogout'),
         [
-          { text: this.props.getLocalizedText("Yes"), onPress: () => resolve(true) },
-          { text: this.props.getLocalizedText("No"), onPress: () => resolve(false) },
+          { text: props.getLocalizedText("Yes"), onPress: () => resolve(true) },
+          { text: props.getLocalizedText("No"), onPress: () => resolve(false) },
         ],
         { cancelable: false }
       );
@@ -77,31 +60,32 @@ class SettingScreen extends React.Component {
 fetchUserInfo = () => {
   let fb = new Firebase();
   let uid = firebase.auth().currentUser.uid;
-  this._isMounted = true;
+   _isMounted = true;
+
   
   if (uid !== null) {
     console.log("User id >>>>>>>>>: " + uid);
     fb.getUserInfo(uid).on('value', (snapshot) => {
-      if (this._isMounted){
+    
           const exists = (snapshot.val() !== null);
           if(exists || snapshot.exists() || snapshot.val() !== 'undefined'){
-            this.setState({
-              fullName: snapshot.val()?.fullName,
-              babyGender:{
-                male: snapshot.val()?.babyGender?.male,
-                female: snapshot.val()?.babyGender?.female,
-              }, 
-              phoneNumber: snapshot.val()?.phoneNumber,
-              pregnant: snapshot.val()?.pregnant,
-              infant: snapshot.val()?.infant,
-              dob: snapshot.val()?.dob,
-              liveMiami: snapshot.val()?.liveMiami,
-              babyDOB:  snapshot.val()?.babyDOB,
-              screen: 'setting'});
+            if(_isMounted){
+              setFullName(snapshot.val()?.fullName);
+              // setBabyGender({
+              //     male: snapshot.val()?.babyGender?.male,
+              //     female: snapshot.val()?.babyGender?.female,
+              //   }); 
+              setPhoneNumber(snapshot.val()?.phoneNumber);
+              setPregnant(snapshot.val()?.pregnant);
+              setInfant(snapshot.val()?.infant);
+              setdob(snapshot.val()?.dob);
+              setLiveMiami(snapshot.val()?.liveMiami);
+              setBabyDOB(snapshot.val()?.babyDOB);
+            }
             
           }
 
-      }
+      
      
     });
 
@@ -116,31 +100,31 @@ fetchUserInfo = () => {
 
  
 
-onSubmit = (fullName, dob, phoneNumber, infant, pregnant, liveMiami, babyGender, babyDOB) => {
+onSubmit = (fullName, dob, phoneNumber, infant, pregnant, liveMiami, babyDOB) => {
   Haptics.selectionAsync().then();
   let uid = firebase.auth().currentUser.uid;
-  let male = babyGender.male;
-  let female = babyGender.female;
+  // let male = babyGender.male;
+  // let female = babyGender.female;
 
   //this will give you the week and nextWeek fields for the baby birth day messages
-  let babyInfo = this.getNextWeekAndWeekNo();
+  let babyInfo = getNextWeekAndWeekNo();
 
   //this is to check whether infant if is male or female 
-    if (male === true) {
-      female = false;
+    // if (male === true) {
+    //   female = false;
 
-    }else if (female === false) {
-      female = true;
+    // }else if (female === false) {
+    //   female = true;
       
-    } else {
-      male = false;
-    }
+    // } else {
+    //   male = false;
+    // }
 
     //this is if the user choose not infant then setup male, female to false and babyDob, week, weekNext to null
     if (infant === false) {
-      male = false;
-      female = false;
-      babyDOB = null;
+      // male = false;
+      // female = false;
+      // babyDOB = null;
       babyInfo[0] = null;
       babyInfo[1] = null;
     }
@@ -157,8 +141,8 @@ onSubmit = (fullName, dob, phoneNumber, infant, pregnant, liveMiami, babyGender,
 
   if (uid !== null) {
 
-    if (!this.state.fullName || !this.state.phoneNumber || !this.state.dob) {
-      alert(this.props.getLocalizedText("fillOutAllFields"));
+    if (!fullName || !phoneNumber || !dob) {
+      alert(props.getLocalizedText("fillOutAllFields"));
       
     }else{
       
@@ -170,16 +154,16 @@ onSubmit = (fullName, dob, phoneNumber, infant, pregnant, liveMiami, babyGender,
           pregnant: pregnant,
           liveMiami: liveMiami || false,
           babyDOB: babyDOB,
-          babyGender:{
-            male: male,
-            female: female
-          },
+          // babyGender:{
+          //   male: male,
+          //   female: female
+          // },
           nextWeek: babyInfo[0],
           week: babyInfo[1],
   
         }).catch(err => console.log(err));
      
-      window.alert(this.props.getLocalizedText("savedInfo"));
+      window.alert(props.getLocalizedText("savedInfo"));
     }
     
   }else{
@@ -189,19 +173,18 @@ onSubmit = (fullName, dob, phoneNumber, infant, pregnant, liveMiami, babyGender,
   
 };
 
-componentWillUnmount(){
-  this._isMounted = false;
-}
 
+useEffect(() => {
+  fetchUserInfo();
 
-componentDidMount(){
-  this.fetchUserInfo();
-}
+  return () => _isMounted = false;
+ 
+}, []);
 
 getNextWeekAndWeekNo = () => {
-  let babyDOB = new Date(this.state.babyDOB); 
+  let newbabyDOB = new Date(babyDOB); 
   let today = new Date(); 
-  let daysDifference = (today.getTime() - babyDOB.getTime()) / (1000 * 3600 * 24) | 0;
+  let daysDifference = (today.getTime() - newbabyDOB.getTime()) / (1000 * 3600 * 24) | 0;
   let daysTillNextWeek = (7 - daysDifference % 7) % 7;        
   let nextweek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + daysTillNextWeek);
   let nextWeek = (nextweek.getMonth()+1).toString().padStart(2, "0") +'/'+nextweek.getDate().toString().padStart(2, "0") +'/'+ nextweek.getFullYear()
@@ -211,13 +194,12 @@ getNextWeekAndWeekNo = () => {
 }
 
 
- render() {
-   this.datetimeField?console.log(this.datetimeField.isValid()):null
-   const { fullName, dob, phoneNumber, liveMiami, infant, pregnant, babyGender, babyDOB} = this.state;
-    return (
-      <View style={{ flex: 1 }}>
+ datetimeField.current?console.log(datetimeField.current.isValid()):null
+
+return (
+  <View style={{ flex: 1 }}>
          <TouchableHighlight
-            onPress={this.goBack}
+            onPress={goBack}
             underlayColor={"transparent"}
             style={{
               height: appStyles.win.height * 0.04,
@@ -226,19 +208,19 @@ getNextWeekAndWeekNo = () => {
               // marginBottom: '3%',
               width: appStyles.win.width * 0.07,
             }}
-          >
+            >
             <Image
               style={{
                 height: appStyles.win.width * 0.06,
                 width: appStyles.win.width * 0.06,
               }}
               source={goBackImg}
-            />
+              />
           </TouchableHighlight>
           <View style={{ position: 'absolute', right: 30, top: 40}}>
             <AntDesign name="logout" size={30} color={appStyles.pinkColor} onPress={() => {
-                this.AsyncAlert().then((response) => {
-                  response ? this.props.logout() : null;
+                AsyncAlert().then((response) => {
+                  response ? props.logout() : null;
                 });
               }} />
           </View>
@@ -251,24 +233,24 @@ getNextWeekAndWeekNo = () => {
               fontWeight: 'bold',
               alignSelf: 'center',
               paddingTop: 15
-              }}>{this.props.getLocalizedText('welcomeSetting')}</Text>
+              }}>{props.getLocalizedText('welcomeSetting')}</Text>
           </View>
           <View style={{ alignItems: 'center', paddingTop: 25}}>
             <View style={{marginBottom: 15, alignItems: 'center'}}>
-              <Text style={appStyles.blueColor}>{this.props.getLocalizedText("phoneNumberInput")}:</Text>
+              <Text style={appStyles.blueColor}>{props.getLocalizedText("phoneNumberInput")}:</Text>
               <View style={appStyles.TextInput.View}>
                 <TextBox
-                  placeholder={this.props.getLocalizedText("phoneNumberInput")}
+                  placeholder={props.getLocalizedText("phoneNumberInput")}
                   style={appStyles.TextInput.TextInput}
                   value={phoneNumber}
                   keyboardType={"numeric"}
-                  onChangeText={(e)=> this.onChangeText({phoneNumber: e})}
+                  onChangeText={(text)=> setPhoneNumber(text)}
                 />
               </View>
             </View>
 
             <View style={{marginBottom: 15, alignItems: 'center'}}>
-              <Text style={appStyles.blueColor}>{this.props.getLocalizedText("dob")}:</Text>
+              <Text style={appStyles.blueColor}>{props.getLocalizedText("dob")}:</Text>
               <View>
               <TextInputMask
                 type={'datetime'}
@@ -282,80 +264,80 @@ getNextWeekAndWeekNo = () => {
                 }}
                 style={appStyles.TextInputMask}
                 value={dob}
-                placeholder={this.props.getLocalizedText("dob")}
-                onChangeText = {text => {
-                  this.setState({ dob: text })
-                }}
+                placeholder={props.getLocalizedText("dob")}
+                onChangeText = {
+                 (text)=> setdob(text)
+                }
                 // Ref for use of isValid(), like this.datetimeField.isValid()
-                ref={(ref) => this.datetimeField = ref}
+                ref={(ref) => datetimeField.current = ref}
               />
                 
               </View>
             </View>
             <View style={{marginBottom: 10, alignItems: 'center' }}>
-              <Text style={appStyles.blueColor}>{this.props.getLocalizedText("fullName")}:</Text>
+              <Text style={appStyles.blueColor}>{props.getLocalizedText("fullName")}:</Text>
               <View style={appStyles.TextInput.View}>
                 <TextBox
-                  placeholder={this.props.getLocalizedText("fullName")}
+                  placeholder={props.getLocalizedText("fullName")}
                   style={appStyles.TextInput.TextInput}
                   value={fullName}
-                  onChangeText={(e)=> this.onChangeText({fullName: e})}
+                  onChangeText={(text) => setFullName(text)}
                 />
               </View>
             </View>
             <View style={styles.containerDropDown}>
-            <Text >{this.props.getLocalizedText("liveMiami")}</Text>
+            <Text >{props.getLocalizedText("liveMiami")}</Text>
                 <Picker
                     selectedValue={liveMiami}
                     style={styles.questionsDropDown}
                     onValueChange={(itemValue, itemIndex) =>
-                      this.onChangeText({liveMiami: itemValue})
+                      setLiveMiami(itemValue)
                     }>
-                    <Picker.Item label={this.props.getLocalizedText("Yes")} value={true} />
-                    <Picker.Item label={this.props.getLocalizedText("No")} value={false} />
+                    <Picker.Item label={props.getLocalizedText("Yes")} value={true} />
+                    <Picker.Item label={props.getLocalizedText("No")} value={false} />
                   </Picker>
             </View>
             <View style={styles.containerDropDown}>
-                  <Text >{this.props.getLocalizedText("areYouPregnant")}</Text>
+                  <Text >{props.getLocalizedText("areYouPregnant")}</Text>
                 <Picker
                     selectedValue={pregnant}
                     style={styles.questionsDropDown}
                     onValueChange={(itemValue, itemIndex) =>
-                      this.onChangeText({pregnant: itemValue})
+                      setPregnant(itemValue)
                     }>
-                    <Picker.Item label={this.props.getLocalizedText("Yes")} value={true} />
-                    <Picker.Item label={this.props.getLocalizedText("No")} value={false} />
+                    <Picker.Item label={props.getLocalizedText("Yes")} value={true} />
+                    <Picker.Item label={props.getLocalizedText("No")} value={false} />
                   </Picker>
             </View>
             <View style={styles.containerDropDown}>
-                  <Text >{this.props.getLocalizedText("doYouHaveInfants")}</Text>
+                  <Text >{props.getLocalizedText("doYouHaveInfants")}</Text>
                 <Picker
                     selectedValue={infant}
                     style={styles.questionsDropDown}
                     onValueChange={(itemValue, itemIndex) =>
-                      this.onChangeText({infant: itemValue})
+                      setInfant(itemValue)
                     }>
-                    <Picker.Item label={this.props.getLocalizedText("Yes")} value={true} />
-                    <Picker.Item label={this.props.getLocalizedText("No")} value={false} />
+                    <Picker.Item label={props.getLocalizedText("Yes")} value={true} />
+                    <Picker.Item label={props.getLocalizedText("No")} value={false} />
                   </Picker>
             </View>
-            {infant === true ? 
+            {/*{infant === true ? 
             <View style={styles.containerDropDown}>
-                  <Text >{this.props.getLocalizedText("selectGenders")}</Text>
-                <Picker
+                  <Text >{props.getLocalizedText("selectGenders")}</Text>
+                 <Picker
                     selectedValue={(babyGender.male && babyGender.female)}
                     style={styles.questionsDropDown}
                     onValueChange={(itemValue, itemIndex) =>{
-                        return this.setState({babyGender:{male: itemValue, female: itemValue}})
+                        return setBabyGender({babyGender:{male: itemValue, female: itemValue}})
                     }}>
-                    <Picker.Item label={this.props.getLocalizedText("Male")} value={true} key='1' />
-                    <Picker.Item label={this.props.getLocalizedText("Female")} value={false}  key='2'/>
-                 </Picker>
+                    <Picker.Item label={props.getLocalizedText("Male")} value={true} key='1' />
+                    <Picker.Item label={props.getLocalizedText("Female")} value={false}  key='2'/>
+                 </Picker> 
             </View>
-            : null}
+              : null} */}
             {infant === true ?
             <View >
-              <Text style={{alignSelf: 'center'}}>{this.props.getLocalizedText("babydob")} {babyDOB}</Text>
+              <Text style={{alignSelf: 'center'}}>{props.getLocalizedText("babydob")}</Text>
               <TextInputMask
                 type={'datetime'}
                 options={{
@@ -367,30 +349,21 @@ getNextWeekAndWeekNo = () => {
                 }}
                 style={appStyles.TextInputMask}
                 value={babyDOB}
-                placeholder={this.props.getLocalizedText("dob")}
-                onChangeText = {text => {
-                  this.setState({ babyDOB: text })
-                }}
+                placeholder={props.getLocalizedText("dob")}
+                onChangeText = {
+                  (text)=> setBabyDOB(text)
+                }
                 // Ref for use of isValid(), like this.dateBaby.isValid()
-                ref={(ref) => this.dateBaby = ref}
+                ref={(ref) => dateBaby = ref}
               />
-                  {/* <TextInput
-                  placeholder={this.props.getLocalizedText("dob")}
-                  style={appStyles.TextInput.TextInput}
-                  // value={babyDOB}
-                  type={'date'}
-                  keyboardType={"numeric"}
-                  dob = {"baby"}
-                  onChangeText={(e)=> this.onChangeText({babyDOB: e})}
-                /> */}
             </View>
             : null}
         
           </View>
           <View style={{justifyContent: 'center', flexDirection: 'row', padding: 90}}>
             <TouchableHighlight style={appStyles.button.TouchableHighlight} underlayColor={appStyles.blueColor}  
-            onPress={() => this.onSubmit(fullName, dob, phoneNumber, infant, pregnant, liveMiami, babyGender, babyDOB)} >
-            <Text style={appStyles.button.text}>{this.props.getLocalizedText("save")}</Text>
+            onPress={() => onSubmit(fullName, dob, phoneNumber, infant, pregnant, liveMiami, babyDOB)} >
+            <Text style={appStyles.button.text}>{props.getLocalizedText("save")}</Text>
             </TouchableHighlight>
 
             
@@ -398,7 +371,7 @@ getNextWeekAndWeekNo = () => {
         </ScrollView>
       </View>
     );
-  }
+  
 }
 
 
