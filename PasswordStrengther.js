0d115d5139fda0e0c90f60dced2117b2c17223f4
LeonelADiaz <leonel.diaz@aol.com>
2020-09-24 16:39:43 -0400

    Minor: Verifies Upper & lower casing, & numbers

--- PasswordStrengther.js
+++ PasswordStrengther.js
@@ -1,26 +1,55 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, TextInput} from 'react-native';
 
+
 const displayCheckPassword = (list) => {
   let stre = list[0]
-  let col = list[1]
-  
+  let streCol = list[1]
+  let numCol = list[2]
+  let upperCol = list[3]
+  let lowerCol = list[4]
+
   const styles = StyleSheet.create({
     strength: {
-      color: col
+      color: streCol
+    },
+    num: {
+      color: numCol
+    },
+    upper: {
+      color: upperCol
+    },
+    lower: {
+      color: lowerCol
     }
   });
  
   return (
-    <Text style={styles.strength}>
+    <View>
+      <Text style={styles.strength}>
        {stre}
-    </Text>
+      </Text>
+      <Text style={styles.num}>
+       Password must have Numbers: [0-9]
+      </Text>
+      <Text style={styles.upper}>
+       Password must have Upper Casing: [A-Z]
+      </Text>
+      <Text style={styles.lower}>
+       Password must have Lower Casing: [a-z]
+      </Text>
+    </View>
+    
   );
 
 }
 const checkPassword = (password) => {
   let passwordLength = password.length
-  let list = [2]
+  let list = [5]
+  let numPatt = /[0-9]/g
+  let upperPatt = /[A-Z]/g 
+  let lowerPatt = /[a-z]/g
+  
   if(passwordLength >= 1 && passwordLength <= 4)
   {
     list = ["Too easy", 'red']
@@ -41,6 +70,35 @@ const checkPassword = (password) => {
   {
     list = ["Please type a password", 'black']
   }
+
+  if(password.match(numPatt) == null)
+  {
+    list[2] = "red"
+  }
+  else
+  {
+    list[2] = "blue"
+  }
+
+  if(password.match(upperPatt) == null)
+  {
+    list[3] = "red"
+  }
+  else
+  {
+    list[3] = "blue"
+  }
+
+  if(password.match(lowerPatt) == null)
+  {
+    list[4] = "red"
+  }
+  else
+  {
+    list[4] = "blue"
+  }
+
+
   return displayCheckPassword(list)
 }
 
