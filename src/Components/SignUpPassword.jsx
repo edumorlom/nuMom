65e5163cd5e35688b7cf2bcb1588a6c7fec278a7
David Antunez <antunezd29@hotmail.com>
2020-09-25 15:41:15 -0400

    working password checker

--- src/Components/SignUpPassword.jsx
+++ src/Components/SignUpPassword.jsx
@@ -8,9 +8,9 @@ export default SignUpPassword = (props) => {
 
     const [password, setPassword] = useState('');
     const [repeat, setRepeat] = useState('');
-    //Sets the default message to sell the user to type a password 
+    //Sets the default message to tell the user to type a password 
     const [passwordLength, setPasswordLength] = useState('Type a password.')
-    //Sets the default password color to black
+    //Sets the default password 
     const [passwordColor, setPasswordColor] = useState('black')
     
     
@@ -20,20 +20,7 @@ export default SignUpPassword = (props) => {
         }).done();
         AsyncStorage.getItem('repeat').then((value) => {
             value !== null && value !== '' ? setRepeat(value) : null;
-        }).done();
-        AsyncStorage.getItem('lengthMessage').then((value) => {
-            if (password.length >= 1 && password.length <= 4) setPasswordLength('Too easy.');
-            else if (password.length >= 5 && password.length <= 9) setPasswordLength('Good.');
-            else if (password.length >= 10 && password.length <= 14) setPasswordLength('Very Good.');
-            else if (password.length >= 15) setPasswordLength('Excellent!') ;
-        }).done();
-        AsyncStorage.getItem('color').then((value) => {
-            if (password.length >= 1 && password.length <= 4) setPasswordColor('lightgreen');
-            else if (password.length >= 5 && password.length <= 9) setPasswordColor('green');
-            else if (password.length >= 10 && password.length<= 14) setPasswordColor('darkgreen');
-            else if (password.length >= 15) setPasswordColor('blue') ;
-        }).done();
-    
+        }).done();  
     }, [])
 
     let onPress = () => {
@@ -44,19 +31,27 @@ export default SignUpPassword = (props) => {
         } else if (password.length < 6){
             alert(translate("passwordTooShort"))
         } else if (password == "password" || password == "qwerty"){
-            alert("Make your password a bit more original to avoid your account from being stolen")
+            alert("Make your password original to avoid your account from being stolen")
         } else {
             props.setUserInfo({password: password});
             AsyncStorage.setItem('pass', password);
             AsyncStorage.setItem('repeat', repeat);
-            AsyncStorage.setItem('lengthMessage',passwordLength);
-            AsyncStorage.setItem('color',passwordColor);
             props.getNextScreen();
         }
     };
-
-
-
+    const passwordChecker = (password) => {
+        setPassword(password);
+        //Set the message printed under the input textbox depending on the size of the password
+        if (password.length >= 1 && password.length <= 4) setPasswordLength('Too easy.');
+        else if (password.length >= 5 && password.length <= 9) setPasswordLength('Good.');
+        else if (password.length >= 10 && password.length <= 14) setPasswordLength('Very Good.');
+        else if (password.length >= 15) setPasswordLength('Excellent!');
+        //Set the color printed under the input textbox depending on the size of the password
+        if (password.length >= 1 && password.length <= 4) setPasswordColor('lightgreen');
+        else if (password.length >= 5 && password.length <= 9) setPasswordColor('green');
+        else if (password.length >= 10 && password.length<= 14) setPasswordColor('darkgreen');
+        else if (password.length >= 15) setPasswordColor('blue');
+    };
         return (
             <TouchableHighlight onPress={Keyboard.dismiss} underlayColor={"transparent"} accessible={false} style={appStyles.container}>
             <>
@@ -68,11 +63,11 @@ export default SignUpPassword = (props) => {
                         <Text style={appStyles.titleBlue}>{translate("createPassword")}</Text>
                         <View style={{paddingTop: appStyles.win.height * 0.1}}>
                             
-                            <TextBox placeholder={translate("passwordInput")} onChangeText={setPassword} secureTextEntry={true} value= {password} style={appStyles.TextInputMask}/>
-                            <Text style={{color: passwordColor,}}>{passwordLength}</Text>
+                            <TextBox placeholder={translate("passwordInput")} onChangeText={passwordChecker} secureTextEntry={true} value= {password} style={appStyles.TextInputMask}/>                            
                             <TextBox placeholder={translate("repeatPasswordInput")} onChangeText={setRepeat} secureTextEntry={true} value= {repeat} style={appStyles.TextInputMask}/>
                             
                         </View>
+                        <Text style={{color: passwordColor}}>{passwordLength}</Text>
                     </View>
                     <View style={{
                         width: '100%',
