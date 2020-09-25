1b8f9c113d5ca53d99140a943d464c05698cd1d9
David Antunez <antunezd29@hotmail.com>
2020-09-25 12:57:19 -0400

    adding password verification

--- src/Components/SignUpPassword.jsx
+++ src/Components/SignUpPassword.jsx
@@ -8,6 +8,11 @@ export default SignUpPassword = (props) => {
 
     const [password, setPassword] = useState('');
     const [repeat, setRepeat] = useState('');
+    //Sets the default message to sell the user to type a password 
+    const [passwordLength, setPasswordLength] = useState('Type a password.')
+    //Sets the default password color to black
+    const [passwordColor, setPasswordColor] = useState('black')
+    
     
     useEffect(() => {
         AsyncStorage.getItem('pass').then((value) => {
@@ -15,6 +20,18 @@ export default SignUpPassword = (props) => {
         }).done();
         AsyncStorage.getItem('repeat').then((value) => {
             value !== null && value !== '' ? setRepeat(value) : null;
+        }).done();
+        AsyncStorage.getItem('lengthMessage').then((value) => {
+            if (password.length >= 1 && password.length <= 4) setPasswordLength('Too easy.');
+            else if (password.length >= 5 && password.length <= 9) setPasswordLength('Good.');
+            else if (password.length >= 10 && password.length <= 14) setPasswordLength('Very Good.');
+            else if (password.length >= 15) setPasswordLength('Excellent!') ;
+        }).done();
+        AsyncStorage.getItem('lengthMessage').then((value) => {
+            if (password.length >= 1 && password.length <= 4) setPasswordColor('lightgreen');
+            else if (password.length >= 5 && password.length <= 9) setPasswordColor('green');
+            else if (password.length >= 10 && password.length<= 14) setPasswordColor('darkgreen');
+            else if (password.length >= 15) setPasswordColor('blue') ;
         }).done();
     
     }, [])
@@ -26,13 +43,20 @@ export default SignUpPassword = (props) => {
             alert(translate("fillOutAllFields"))
         } else if (password.length < 6){
             alert(translate("passwordTooShort"))
+        } else if (password == "password" || password == "qwerty"){
+            alert(translate("Make your password a bit more original to avoid your account from being stolen"))
         } else {
             props.setUserInfo({password: password});
             AsyncStorage.setItem('pass', password);
             AsyncStorage.setItem('repeat', repeat);
+            AsyncStorage.setItem('lengthMessage',passwordLength);
+            AsyncStorage.setItem('color',passwordColor);
             props.getNextScreen();
         }
     };
+
+
+
         return (
             <TouchableHighlight onPress={Keyboard.dismiss} underlayColor={"transparent"} accessible={false} style={appStyles.container}>
             <>
@@ -44,8 +68,8 @@ export default SignUpPassword = (props) => {
                         <Text style={appStyles.titleBlue}>{translate("createPassword")}</Text>
                         <View style={{paddingTop: appStyles.win.height * 0.1}}>
                             
-                            <TextBox placeholder={translate("passwordInput")} onChangeText={setPassword} secureTextEntry={true} value= {password} style={appStyles.TextInputMask}/>
-                        
+                            <TextBox placeholder={translate("passwordInput")} onChangeText={setPassword,setPasswordColor,setPasswordLength} secureTextEntry={true} value= {password} style={appStyles.TextInputMask}/>
+                            <Text style={{color: passwordColor,}}>{passwordLength}</Text>
                             <TextBox placeholder={translate("repeatPasswordInput")} onChangeText={setRepeat} secureTextEntry={true} value= {repeat} style={appStyles.TextInputMask}/>
                             
                         </View>
