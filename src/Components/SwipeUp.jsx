9b74bfd63edd9dc0c3237f3f87a22d8d937542cc
LeonelADiaz <leonel.diaz@aol.com>
2020-10-06 17:53:40 -0400

    #132 Progress. Works but TouchableHighlight removed from Login.jsx. Interferes with GestureRecognizer

--- src/Components/SwipeUp.jsx
+++ src/Components/SwipeUp.jsx
@@ -1,9 +1,11 @@
-import {Image, Text} from "react-native";
+import {Image, Text, Platform, View, KeyboardAvoidingView} from "react-native";
 import appStyles from "./AppStyles";
 import React from "react";
 import * as Haptics from "expo-haptics";
 import GestureRecognizer from "react-native-swipe-gestures";
 import swipeUpGif from "../../assets/swipe-up-arrow.gif";
+import Button from "./Button";
+import translate from "app/Components/getLocalizedText";
 
 export default function SwipeUp(props){
 
@@ -15,8 +17,9 @@ export default function SwipeUp(props){
     return (
         <GestureRecognizer
             onSwipeUp={ () => onSwipeUp()}
-            config={{velocityThreshold: 0.4, directionalOffsetThreshold: 100}}
-            style={{position: 'absolute', width: appStyles.win.width, paddingTop: appStyles.win.height * 0.2, bottom: '3%', alignItems: 'center'}}>
+            config={{velocityThreshold: 0.1, directionalOffsetThreshold: 100}}
+            style={{position: 'absolute', width: appStyles.win.width, paddingTop: appStyles.win.height * 0.2, bottom: '-50%', alignItems: 'center'}}
+            >
                 <Image style={{width: 30, height: 30, margin: 8}} source={swipeUpGif}/>
                 <Text style={{...appStyles.paragraphText, color: appStyles.blueColor, textAlign: 'center'}}>{props.text}</Text>
         </GestureRecognizer>
