2375f806ab6a3416235c2106e066cfd375ef031a
LeonelADiaz <leonel.diaz@aol.com>
2020-10-07 00:47:37 -0400

    Update SignUpPassword.jsx
    Removed my changes to work with Pull Request.

--- src/Components/SignUpPassword.jsx
+++ src/Components/SignUpPassword.jsx
@@ -1,116 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { AsyncStorage, Keyboard, StyleSheet, Text, TextInput as TextBox, TouchableOpacity, View, TouchableHighlight } from 'react-native';
+import { AsyncStorage, Keyboard, Text, TextInput as TextBox, TouchableOpacity, View, TouchableHighlight } from 'react-native';
 import appStyles from './AppStyles';
 import Button from "./Button";
 import translate from "app/Components/getLocalizedText";
-//import checkPassword from "app/Components/PasswordSecurity"
 
 export default SignUpPassword = (props) => {
 
     const [password, setPassword] = useState('');
     const [repeat, setRepeat] = useState('');
-
-    const displayCheckPassword = (list) => {
-        let stre = list[0]
-        let streCol = list[1]
-        let numCol = list[2]
-        let upperCol = list[3]
-        let lowerCol = list[4]
-      
-        const styles = StyleSheet.create({
-          strength: {
-            color: streCol
-          },
-          num: {
-            color: numCol
-          },
-          upper: {
-            color: upperCol
-          },
-          lower: {
-            color: lowerCol
-          }
-        });
-       
-        return (
-          <View>
-            <Text style={styles.strength}>
-             {stre}
-            </Text>
-            <Text style={styles.num}>
-             Password must have Numbers: [0-9]
-            </Text>
-            <Text style={styles.upper}>
-             Password must have Upper Casing: [A-Z]
-            </Text>
-            <Text style={styles.lower}>
-             Password must have Lower Casing: [a-z]
-            </Text>
-          </View>
-          
-        );
-      
-      }
-      
-      const checkPassword = (password) => {
-        let passwordLength = password.length
-        let list = [5]
-        let numPatt = /[0-9]/g
-        let upperPatt = /[A-Z]/g 
-        let lowerPatt = /[a-z]/g
-        
-        if(passwordLength >= 1 && passwordLength <= 4)
-        {
-          list = ["Too easy", 'red']
-        }
-        else if(passwordLength >= 5 && passwordLength <= 9)
-        {
-          list = ["Good", 'orange']
-        }
-        else if(passwordLength >= 10 && passwordLength <= 14)
-        {
-          list = ["Very Good", 'green']
-        }
-        else if(passwordLength >= 15)
-        {
-          list = ["Excellent", 'blue']
-        }
-        else
-        {
-          list = ["Please type a password", 'black']
-        }
-      
-        if(password.match(numPatt) == null)
-        {
-          list[2] = "red"
-        }
-        else
-        {
-          list[2] = "blue"
-        }
-      
-        if(password.match(upperPatt) == null)
-        {
-          list[3] = "red"
-        }
-        else
-        {
-          list[3] = "blue"
-        }
-      
-        if(password.match(lowerPatt) == null)
-        {
-          list[4] = "red"
-        }
-        else
-        {
-          list[4] = "blue"
-        }
-      
-      
-        return displayCheckPassword(list)
-    }
-
+    
     useEffect(() => {
         AsyncStorage.getItem('pass').then((value) => {
             value !== null && value !== '' ? setPassword(value) : null;
@@ -120,11 +18,6 @@ export default SignUpPassword = (props) => {
         }).done();
     
     }, [])
-
-    let multiInput = () => {
-        this.setPassword;
-        checkPassword(password);
-    }
 
     let onPress = () => {
         if (password !== repeat) {
@@ -154,7 +47,6 @@ export default SignUpPassword = (props) => {
                             <TextBox placeholder={translate("passwordInput")} onChangeText={setPassword} secureTextEntry={true} value= {password} style={appStyles.TextInputMask}/>
                         
                             <TextBox placeholder={translate("repeatPasswordInput")} onChangeText={setRepeat} secureTextEntry={true} value= {repeat} style={appStyles.TextInputMask}/>
-
                             
                         </View>
                     </View>
