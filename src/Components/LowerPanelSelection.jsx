68259cd497c770a1741203438856643a3c661680
yhern185 <yhern185@fiu.edu>
2020-10-21 17:07:02 -0400

    Delete buttons Clinics and Shelter. Added Facilities

--- src/Components/LowerPanelSelection.jsx
+++ src/Components/LowerPanelSelection.jsx
@@ -23,69 +23,70 @@ import Button from "./Button";
 export default function LowerPanelSelection(props) {
   return (
     <>
-    <GestureRecognizer
-      onSwipeUp={() => props.setFullPanel(true)}
-      onSwipeDown={() => props.setFullPanel(false)}
-      style={{ 
-        width: appStyles.win.width, 
-        height: appStyles.win.height * 0.16, 
-        alignItems: "center" 
-      }}
-    >
-      <Button 
-      style= {{
-        Touchable: {height: appStyles.win.height * 0.06, width: appStyles.win.width}, Text: {}
-      }}
-      text={''}
-      underlayColor={"transparent" }
-      onPress={() => props.setFullPanel(!props.fullPanel)}
-      />
-      <WelcomeUserBanner
-        fullName={props.fullName}
-        logout={props.logout}
-      />
-      <View>
-        <MaterialIcons
-          name="settings"
-          size={45}
-          color= "gray"
-          style={styles.userSettingStyle}
-          onPress={() => props.setScreen("setting")}
+      <GestureRecognizer
+        onSwipeUp={() => props.setFullPanel(true)}
+        onSwipeDown={() => props.setFullPanel(false)}
+        style={{
+          width: appStyles.win.width,
+          height: appStyles.win.height * 0.16,
+          alignItems: "center",
+        }}
+      >
+        <Button
+          style={{
+            Touchable: {
+              height: appStyles.win.height * 0.06,
+              width: appStyles.win.width,
+            },
+            Text: {},
+          }}
+          text={""}
+          underlayColor={"transparent"}
+          onPress={() => props.setFullPanel(!props.fullPanel)}
         />
-      </View>
+        <WelcomeUserBanner fullName={props.fullName} logout={props.logout} />
+        <View>
+          <MaterialIcons
+            name="settings"
+            size={45}
+            color="gray"
+            style={styles.userSettingStyle}
+            onPress={() => props.setScreen("setting")}
+          />
+        </View>
       </GestureRecognizer>
-      <ScrollView contentContainerStyle={{alignItems: 'center', width: appStyles.win.width}}>
-      <SelectionButton
-        style={appStyles.PanelSelectionButton}
-        text={translate("findCare")}
-        icon={clinicLogo}
-        onPress={() => props.setLowerPanelContent("findCare")}
-      />
-      <SelectionButton
-        style={appStyles.PanelSelectionButton}
-        text={translate("shelters")}
-        icon={shelterLogo}
-        onPress={() => props.setLowerPanelContent("shelters")}
-      />
-      <SelectionButton
-        style={appStyles.PanelSelectionButton}
-        text={translate("learn")}
-        icon={babyBottle}
-        onPress={() => {
-          props.setLowerPanelContent("learn");
+      <ScrollView
+        contentContainerStyle={{
+          alignItems: "center",
+          width: appStyles.win.width,
         }}
-      />
-      <SelectionButton
-        style={appStyles.PanelSelectionButton}
-        text={translate("resources")}
-        icon={lightBulb}
-        onPress={() => {
-          props.setLowerPanelContent("resources");
-        }}
-      />
+      >
+        <SelectionButton
+          style={appStyles.PanelSelectionButton}
+          text={translate("facilities")}
+          icon={clinicLogo}
+          onPress={() => {
+            props.setLowerPanelContent("facilities");
+          }}
+        />
+        <SelectionButton
+          style={appStyles.PanelSelectionButton}
+          text={translate("learn")}
+          icon={babyBottle}
+          onPress={() => {
+            props.setLowerPanelContent("learn");
+          }}
+        />
+        <SelectionButton
+          style={appStyles.PanelSelectionButton}
+          text={translate("resources")}
+          icon={lightBulb}
+          onPress={() => {
+            props.setLowerPanelContent("resources");
+          }}
+        />
       </ScrollView>
-      </>
-    
+    </>
   );
 }
 
@@ -93,8 +94,7 @@ const styles = StyleSheet.create({
   userSettingStyle: {
     position: "absolute",
     left: appStyles.win.width * 0.27,
-    bottom: appStyles.win.height * 0.030,
-    color: '#706e6c'
-    
+    bottom: appStyles.win.height * 0.03,
+    color: "#706e6c",
   },
 });
