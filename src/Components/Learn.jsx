448fe9f0eae3b4c33969f6824d2dea4c7ac2a5bc
PantherDave <dacos062@fiu.edu>
2020-10-21 01:22:53 -0400

    Completely removes old STD awareness logo

--- src/Components/Learn.jsx
+++ src/Components/Learn.jsx
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 import { TouchableHighlight, Text, View, ScrollView } from "react-native";
 import appStyles from "./AppStyles";
 import SelectionButton from "./SelectionButton";
-import butterfly from "../../assets/butterfly.png";
 import STDAwareness from "../../assets/std-awareness.png";
 import translate from "./getLocalizedText";
 import FemaleCondom from "../../assets/FC.png";
-=======
-import React from 'react';
-import {TouchableHighlight, Text, View, ScrollView} from 'react-native';
-import appStyles from './AppStyles';
-import SelectionButton from './SelectionButton';
-import butterfly from '../../assets/butterfly.png';
-import translate from './getLocalizedText';
-import FemaleCondom from '../../assets/FC.png';
->>>>>>> c7aa10f4d527f5516563630e3ffb69fa0879d82a
+
 
 export default function learn(props) {
   return (
@@ -24,15 +14,9 @@ export default function learn(props) {
     >
       <SelectionButton
         style={appStyles.ImageOnRightSelectionButton}
-<<<<<<< HEAD
         text={translate("STDAwareness")}
         subtext={translate("learnSTDs")}
         icon={STDAwareness}
-=======
-        text={translate('STDAwareness')}
-        subtext={translate('learnSTDs')}
-        icon={butterfly}
->>>>>>> c7aa10f4d527f5516563630e3ffb69fa0879d82a
         onPress={() => {
           props.setLowerPanelContent('STDSelection');
         }}
