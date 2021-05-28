21d7b7e2cde261a7b1921445e0991ddbcf90245a
Eric Vilella <evile003@fiu.edu>
2021-05-27 20:51:05 -0400

    Altered the password file to check the strength of the password. Currently Doesn't check common passwords, and it is a little behind.

--- src/Components/SignUpPassword.jsx
+++ src/Components/SignUpPassword.jsx
@@ -22,6 +22,57 @@ export default SignUpPassword = (props) => {
   const {email} = props.route.params;
   const {phone} = props.route.params;
 
+  const [passwordStrength, setPasswordStrength] = useState();
+
+  const strengthDisplay = () => {
+    if (passwordStrength === 0) {
+      return (
+        <Text style={{color: appStyles.pinkColor}}>
+          {' '}
+          Password Strength: Poor
+        </Text>
+      );
+    }
+    if (passwordStrength === 1) {
+      return (
+        <Text style={{color: appStyles.blueColor}}>
+          {' '}
+          Password Strength: Medium
+        </Text>
+      );
+    }
+    if (passwordStrength === 2) {
+      return <Text style={{color: '#298000'}}> Password Strength: High</Text>;
+    }
+  };
+
+  const passwordTypes = () => {
+    let counter = 0;
+    if (/([A-Z])/i.test(password)) {
+      counter++;
+    }
+    if (/([0-9])/i.test(password)) {
+      counter++;
+    }
+    if (/([!@#$%^&*])/i.test(password)) {
+      counter++;
+    }
+    if (/([A-Z])/.test(password)) {
+      counter++;
+    }
+    return counter;
+  };
+
+  const checkPasswordStrength = () => {
+    if (password.length <= 4) {
+      setPasswordStrength(0);
+    } else if (password.length >= 5 && passwordTypes() === 3) {
+      setPasswordStrength(1);
+    } else if (password.length >= 5 && passwordTypes() === 4) {
+      setPasswordStrength(2);
+    }
+  };
+
   useEffect(() => {
     AsyncStorage.getItem('pass')
       .then((value) => {
@@ -40,7 +91,10 @@ export default SignUpPassword = (props) => {
       alert(translate('passwordMismatch'));
     } else if (!password || !repeat) {
       alert(translate('fillOutAllFields'));
-    } else if (password.length < 6) {
+    } else if (passwordStrength === 0) {
+      alert('Password is not strong enough!');
+    } else if (password.length < 5) {
+      // Backup incase password checker not updated properly
       alert(translate('passwordTooShort'));
     } else {
       // props.setUserInfo({password});
@@ -86,11 +140,12 @@ export default SignUpPassword = (props) => {
                 <TextBox
                   placeholder={translate('passwordInput')}
                   onChangeText={setPassword}
+                  onKeyPress={checkPasswordStrength}
                   secureTextEntry
                   value={password}
                   style={appStyles.TextInputMask}
                 />
-
+                {strengthDisplay()}
                 <TextBox
                   placeholder={translate('repeatPasswordInput')}
                   onChangeText={setRepeat}
