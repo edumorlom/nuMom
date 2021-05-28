eb15edc02d618e1e45f7973d10f9e10816d318c7
Nat <70817822+NathalyS19@users.noreply.github.com>
2021-05-28 10:45:45 -0400

    Password Issue #271 Progress

--- src/Components/SignUpPassword.jsx
+++ src/Components/SignUpPassword.jsx
@@ -12,10 +12,12 @@ import {
 import appStyles from './AppStyles';
 import Button from './Button';
 import translate from './getLocalizedText';
+import listOfPasswords from './PoorPasswords';
 
 export default SignUpPassword = (props) => {
   const [password, setPassword] = useState('');
   const [repeat, setRepeat] = useState('');
+  const [passwordStrength, setPasswordStrength] = useState(0);
   const {liveMiami} = props.route.params;
   const {name} = props.route.params;
   const {dob} = props.route.params;
@@ -35,14 +37,101 @@ export default SignUpPassword = (props) => {
       .done();
   }, []);
 
+  const passwordCheckType = () => {
+    let typeStrength = 0;
+    let typeNumber = new RegExp('(?=.*[0-9])');
+    let typeCapital = new RegExp('(?=.*[A-Z])');
+    let typeSymbol = new RegExp('(?=.*[^A-Za-z0-9])');
+    let typeLetter = new RegExp('(?=.*[a-z])');
+
+    if (typeNumber.test(password)) {
+      typeStrength++;
+    }
+    if (typeCapital.test(password)) {
+      typeStrength++;
+    }
+    if (typeSymbol.test(password)) {
+      typeStrength++;
+    }
+    if (typeLetter.test(password)) {
+      typeStrength++;
+    }
+
+    return typeStrength;
+  }
+  
+  const checkPasswordStrength = () => {
+    let poorPasswords = listOfPasswords.split('\n');
+
+    if (password.length <= 4 || poorPasswords.includes(password)) {
+      setPasswordStrength(0);
+    }
+    else if (password.length >= 5 && passwordCheckType() == 3) {
+      setPasswordStrength(1);
+    }
+    else if (password.length >= 5 && passwordCheckType() == 4) {
+      setPasswordStrength(2);
+    }
+  }
+
+  const passwordStrengthView = () => {
+    if (passwordStrength === 0) {
+      return (
+        <Text
+            style={{
+              color: appStyles.pinkColor,
+              fontSize: appStyles.regularFontSize - 3,
+              fontWeight: 'bold',
+            }}
+          >
+            {translate('poorPassword')}
+          </Text>
+      );
+    }
+    if (passwordStrength === 1) {
+      return (
+        <Text
+            style={{
+              color: appStyles.blueColor,
+              fontSize: appStyles.regularFontSize - 3,
+              fontWeight: 'bold',
+            }}
+          >
+            {translate('mediumPassword')}
+          </Text>
+      );
+    }
+    if (passwordStrength === 2) {
+      return (
+        <Text
+            style={{
+              color: '#298000',
+              fontSize: appStyles.regularFontSize - 3,
+              fontWeight: 'bold',
+            }}
+          >
+            {translate('highPassword')}
+          </Text>
+      )
+    }
+  }
+
+  const onChangePassword = (password) => {
+    checkPasswordStrength();
+    setPassword(password);
+  }
+
   let onPress = () => {
     if (password !== repeat) {
       alert(translate('passwordMismatch'));
     } else if (!password || !repeat) {
       alert(translate('fillOutAllFields'));
-    } else if (password.length < 6) {
+    } else if (password.length <= 4) {
       alert(translate('passwordTooShort'));
-    } else {
+    } else if (passwordStrength === 0)  {
+      alert(translate('passwordNotStrong'));
+    }
+    else {
       // props.setUserInfo({password});
       // AsyncStorage.setItem('pass', password);
       // AsyncStorage.setItem('repeat', repeat);
@@ -85,11 +174,16 @@ export default SignUpPassword = (props) => {
               <View style={{paddingTop: appStyles.win.height * 0.05}}>
                 <TextBox
                   placeholder={translate('passwordInput')}
-                  onChangeText={setPassword}
+                  onChangeText={setPassword} 
+                  onChangeText={onChangePassword}
                   secureTextEntry
                   value={password}
                   style={appStyles.TextInputMask}
                 />
+
+                <Text style={{color: appStyles.darkGreyColor, fontWeight: 'normal'}}>
+                  Password Strength: {passwordStrengthView()}
+                </Text>
 
                 <TextBox
                   placeholder={translate('repeatPasswordInput')}
