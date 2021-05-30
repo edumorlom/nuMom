3172a147070e3a4c351597c80ecec7300b48efbc
Estephany Sanchez <esanc218@fiu.edu>
2021-05-30 12:50:18 -0400

    working on password strength

--- src/Components/SignUpPassword.jsx
+++ src/Components/SignUpPassword.jsx
@@ -12,6 +12,7 @@ import {
 import appStyles from './AppStyles';
 import Button from './Button';
 import translate from './getLocalizedText';
+import PoorPasswords from './PoorPasswords';
 
 export default SignUpPassword = (props) => {
   const [password, setPassword] = useState('');
@@ -21,6 +22,64 @@ export default SignUpPassword = (props) => {
   const {dob} = props.route.params;
   const {email} = props.route.params;
   const {phone} = props.route.params;
+
+  const passwordStrengthShow = (passwordStrength) => {
+    // console.log(passwordStrength);
+
+    if (passwordStrength === 0) {
+      return (
+        <Text style={{color: appStyles.pinkColor}}>
+          Password Strength: Poor
+        </Text>
+      );
+    }
+    if (passwordStrength === 1) {
+      return (
+        <Text style={{color: appStyles.blueColor}}>
+          Password Strength: Medium
+        </Text>
+      );
+    }
+    if (passwordStrength === 2) {
+      return <Text style={{color: '#298000'}}> Password Strength: High</Text>;
+    }
+  };
+
+  // function to check the strengh of the password
+  const checkPasswordStrength = (password) => {
+    let amount = 0;
+    let poorPasswordList = PoorPasswords.split('\n');
+
+    if (/([A-Z])/i.test(password)) {
+      amount++;
+    }
+    if (/([0-9])/i.test(password)) {
+      amount++;
+    }
+    if (/([!@#$%^&*])/i.test(password)) {
+      amount++;
+    }
+    if (/([a-z])/.test(password)) {
+      amount++;
+    }
+    if (password.length <= 4 || poorPasswordList.includes(password)) {
+      return 0;
+    }
+    if (
+      password.length >= 5 &&
+      !poorPasswordList.includes(password) &&
+      amount === 3
+    ) {
+      return 1;
+    }
+    if (
+      password.length >= 5 &&
+      !poorPasswordList.includes(password) &&
+      amount === 4
+    ) {
+      return 2;
+    }
+  };
 
   useEffect(() => {
     AsyncStorage.getItem('pass')
@@ -35,12 +94,15 @@ export default SignUpPassword = (props) => {
       .done();
   }, []);
 
-  let onPress = () => {
+  let onPress = (passwordStrength) => {
     if (password !== repeat) {
       alert(translate('passwordMismatch'));
     } else if (!password || !repeat) {
       alert(translate('fillOutAllFields'));
-    } else if (password.length < 6) {
+    } else if (passwordStrength === 0) {
+      alert('Password is not strong enough!');
+    } else if (password.length < 5) {
+      // Backup incase password checker not updated properly
       alert(translate('passwordTooShort'));
     } else {
       // props.setUserInfo({password});
@@ -58,6 +120,11 @@ export default SignUpPassword = (props) => {
       });
     }
   };
+
+  const pwdStrength = checkPasswordStrength(password);
+  // console.log(pwdStrength);
+  // console.log(password);
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -90,7 +157,7 @@ export default SignUpPassword = (props) => {
                   value={password}
                   style={appStyles.TextInputMask}
                 />
-
+                {passwordStrengthShow(pwdStrength)}
                 <TextBox
                   placeholder={translate('repeatPasswordInput')}
                   onChangeText={setRepeat}
@@ -111,7 +178,9 @@ export default SignUpPassword = (props) => {
             <Button
               style={appStyles.button}
               text={translate('continueButton')}
-              onPress={onPress}
+              onPress={() => {
+                onPress(pwdStrength);
+              }}
             />
           </View>
         </>
