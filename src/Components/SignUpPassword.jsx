7613102d0f1d9df31c3b04ef8a24d572d40fb5b8
lroche823 <lroche823@gmail.com>
2021-09-16 21:33:54 -0400

    password checker solution

--- src/Components/SignUpPassword.jsx
+++ src/Components/SignUpPassword.jsx
@@ -9,18 +9,70 @@ import {
   TouchableHighlight,
   KeyboardAvoidingView,
 } from 'react-native';
-import appStyles from './AppStyles';
+import appStyles, {blueColor, pinkColor} from './AppStyles';
 import Button from './Button';
 import translate from './getLocalizedText';
+import badPasswords from './BadPasswords';
 
 export default SignUpPassword = (props) => {
   const [password, setPassword] = useState('');
   const [repeat, setRepeat] = useState('');
+  const [passwordStrength, setPasswordStrength] = useState(0);
   const {liveMiami} = props.route.params;
   const {name} = props.route.params;
   const {dob} = props.route.params;
   const {email} = props.route.params;
   const {phone} = props.route.params;
+
+  const checkPasswordStrength = (password) => {
+    if(badPasswords.includes(password)){
+      setPasswordStrength(0);
+      return;
+    }
+
+    let typeCount = 0;
+    if(/[A-Z]/.test(password)){ // tests capital
+      typeCount++;
+    }
+    if(/[a-z]/.test(password)){ // tests letter
+      typeCount++;
+    }
+    if(/[0-9]/.test(password)){ // tests number
+      typeCount++;
+    }
+    if(/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password)){ // tests symbol
+      typeCount++;
+    }
+    
+    if(typeCount == 4 && password.length >= 5) {
+      setPasswordStrength(2);
+    } else if(typeCount == 3 && password.length >= 5) {
+      setPasswordStrength(1);
+    } else{
+      setPasswordStrength(0);
+    }
+  }
+
+  const passwordViewStyle = (passwordStrength) => {
+    let textColor;
+    let textMsg;
+    if(passwordStrength == 0){
+      textColor = pinkColor;
+      textMsg = "Password Strength: Poor"
+    } else if (passwordStrength == 1){
+      textColor = blueColor;
+      textMsg = "Password Strength: Medium"
+    } else{
+      textColor = "#298000";
+      textMsg = "Password Strength: High"
+    }
+    return <Text style={{color: textColor,  textAlign:"center"}}>{textMsg}</Text>
+  }
+
+  const onChangePass = (password) => {
+    checkPasswordStrength(password);
+    setPassword(password);
+  }
 
   useEffect(() => {
     AsyncStorage.getItem('pass')
@@ -40,8 +92,10 @@ export default SignUpPassword = (props) => {
       alert(translate('passwordMismatch'));
     } else if (!password || !repeat) {
       alert(translate('fillOutAllFields'));
-    } else if (password.length < 6) {
+    } else if (password.length <= 4) {
       alert(translate('passwordTooShort'));
+    } else if(passwordStrength == 0){
+      alert(translate('passwordWeak'));
     } else {
       // props.setUserInfo({password});
       // AsyncStorage.setItem('pass', password);
@@ -85,12 +139,12 @@ export default SignUpPassword = (props) => {
               <View style={{paddingTop: appStyles.win.height * 0.05}}>
                 <TextBox
                   placeholder={translate('passwordInput')}
-                  onChangeText={setPassword}
+                  onChangeText={onChangePass}
                   secureTextEntry
                   value={password}
                   style={appStyles.TextInputMask}
                 />
-
+                {passwordViewStyle(passwordStrength)}
                 <TextBox
                   placeholder={translate('repeatPasswordInput')}
                   onChangeText={setRepeat}
