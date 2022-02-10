f2e5027a2c4326499c10415007226c683707aab4
Julian Lopez <julianlopez6850@gmail.com>
2022-02-10 02:01:52 -0500

    pw checker finishing touches

--- src/Components/SignUpPassword.jsx
+++ src/Components/SignUpPassword.jsx
@@ -9,7 +9,7 @@ import {
   TouchableHighlight,
   KeyboardAvoidingView,
 } from 'react-native';
-import appStyles, {blueColor, pinkColor} from './AppStyles';
+import appStyles, {blueColor, pinkColor, greenColor} from './AppStyles';
 import Button from './Button';
 import translate from './getLocalizedText';
 import badPasswords from './BadPasswords';
@@ -19,8 +19,13 @@ export default SignUpPassword = (props) => {
   const [password, setPassword] = useState('');
   const [repeat, setRepeat] = useState('');
   const [passwordStrength, setPasswordStrength] = useState('');
-  const [typeCount, setTypeCount] = useState('');
-  const {liveMiami} = props.route.params;
+  const [charCount, setCharCount] = useState('');
+
+  const [isLowercase, setIsLowercase] = useState(false);
+  const [isUppercase, setIsUppercase] = useState(false);
+  const [isNum, setIsNum] = useState(false);
+  const [isSymbol, setIsSymbol] = useState(false);
+
   const {name} = props.route.params;
   const {dob} = props.route.params;
   const {email} = props.route.params;
@@ -37,61 +42,65 @@ export default SignUpPassword = (props) => {
         value !== null && value !== '' ? setRepeat(value) : null;
       })
       .done();
-
-      passwordStyle();
-
     }, []);
-
     
-
-  let checkPassword = () => {
-    if(badPasswords.includes(password))
-    {
-      setPasswordStrength(0);
-      return;
-    }
+  let checkPassword = (password) => {
+    
+    setIsLowercase(false);
+    setIsUppercase(false);
+    setIsNum(false);
+    setIsSymbol(false);
 
     let charCount = 0;
-    for(let i = 0; i < password.length; i++)
-      if(/[A-Z]/.test(password))
-      {
-        charCount += 1000;
-        setTypeCount(charCount);
-        break;
-      }
-    for(let i = 0; i < password.length; i++)
+
+    for(let i = 0; i < password.length; i++)  // check to see if password has a lowercase letter
       if(/[a-z]/.test(password))
       {
-        charCount += 100;
-        setTypeCount(charCount);
+        charCount++;
+        setIsLowercase(true);
         break;
       }
-    for(let i = 0; i < password.length; i++)
+    for(let i = 0; i < password.length; i++)  // check to see if password has a uppercase letter
+      if(/[A-Z]/.test(password))
+      {
+        charCount++;
+        setIsUppercase(true);
+        break;
+      }
+    for(let i = 0; i <= password.length; i++)  // check to see if password has a number
       if(/[0-9]/.test(password))
       {
-        charCount += 10;
-        setTypeCount(charCount);
+        charCount++;
+        setIsNum(true);
         break;
       }
-    for(let i = 0; i < password.length; i++)
+    for(let i = 0; i <= password.length; i++)  // check to see if password has a symbol
       if(/[^A-Za-z0-9 ]/.test(password))
       {
-        charCount += 1;
-        setTypeCount(charCount);
+        charCount++;
+        setIsSymbol(true);
         break;
       }
     
-    if(charCount == 1111)
+    setCharCount(charCount);
+
+      if(badPasswords.includes(password)) // if the password is found in the list of bad passwords, set the passwordStrength to 0 ('poor')
+      {
+        setPasswordStrength(0);
+        return;
+      }
+    
+    if(charCount == 4 && password.length >= 5) // if the criteria for 'high' password strength is met, set passwordStrength to 2
       setPasswordStrength(2)
-    else if(charCount == 1110 || charCount == 1101 || charCount == 1011 || charCount == 111)
+    else if(charCount == 3 && password.length >= 5) // else, if the criteria for 'medium' password strength is met, set passwordStrength to 1
       setPasswordStrength(1)
-    else
+    else                        // otherwise... set the passwordStrength to 0 ('poor')
       setPasswordStrength(0)
 
       
   }
 
-  const onChangePassword = async (password) => {
+  let onChangePassword = (password) => {
     setPassword(password);
     checkPassword(password);
   }
@@ -99,30 +108,67 @@ export default SignUpPassword = (props) => {
   let passwordStyle = (passwordStrength) => {
     let _color;
     let message;
+    let message2 = "";
+    
+    let messageLower = " \t- lowercase letter"
+    let messageUpper = " \t- uppercase letter"
+    let messageNum = " \t- number"
+    let messageSymbol = " \t- symbol"
+
+    let colorLower = pinkColor, colorUpper = pinkColor, colorNum = pinkColor, colorSymbol = pinkColor;
+    
     if(passwordStrength == 0)
     {
       _color = pinkColor;
       message = "Password Strength: Poor"
+      if(password.length <= 4)
+        message2 += " \n* Password must be at least 5 characters"
+      
+        if(!isLowercase && !isUppercase && !isNum && !isSymbol)  // if there are NONE of the character types (uppercase, lowercase, number, symbol)
+        {
+          message2 += "\n* The password should contain atleast 3 of the following:"
+        }
+        else if( charCount == 1 )  // if there is ONE of the character types (uppercase, lowercase, number, symbol)
+        {
+          message2 += "\n* The password should contain atleast 2 more of the following:"
+        }
+        else  // if there are TWO of the character types (uppercase, lowercase, number, symbol)
+        {
+          message2 += "\n* The password should contain atleast 1 more of the following: "
+        }
+      
+      
     }
     if(passwordStrength == 1)
     {
       _color = blueColor;
       message = "Password Strength: Medium"
-      if(typeCount == 1110)
-        message += " (Password is missing a symbol)"
-      if(typeCount == 1101)
-        message += " (Password is missing a number)"
-      if(typeCount == 1011)
-        message += " (Password is missing a lower-case letter)"
-      if(typeCount == 111)
-        message += " (Password is missing a capital letter)"
     }
     if(passwordStrength == 2)
     {
       _color = "#298000";
       message = "Password Strength: High"
     }
-    return <Text style={{color: _color,  textAlign:"center"}}>{message}</Text>
+
+    if(isLowercase)
+      colorLower = "#298000";
+    if(isUppercase)
+      colorUpper = "#298000";
+    if(isNum)
+      colorNum = "#298000";
+    if(isSymbol)
+      colorSymbol = "#298000";
+
+    return (
+      <View >
+        <Text style={{color: _color,  textAlign:"center"}}>{message}</Text>
+        <Text style={{color: _color,  textAlign:"left"}}>{message2}</Text>
+        <Text style={{color: colorLower,  textAlign:"left"}}>{messageLower}</Text>
+        <Text style={{color: colorUpper,  textAlign:"left"}}>{messageUpper}</Text>
+        <Text style={{color: colorNum,  textAlign:"left"}}>{messageNum}</Text>
+        <Text style={{color: colorSymbol,  textAlign:"left"}}>{messageSymbol}</Text>
+      </View>
+    );
   }
 
   let onPress = () => {
@@ -139,7 +185,6 @@ export default SignUpPassword = (props) => {
       // AsyncStorage.setItem('pass', password);
       // AsyncStorage.setItem('repeat', repeat);
       props.navigation.navigate('SignUpYesorNoPregnant', {
-        liveMiami,
         name,
         dob,
         email,
