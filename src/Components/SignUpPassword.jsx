282845bf1f009decd3ba2fe7dd7c884de310d186
Kimberly Metelus <kimberly@Kimberlys-MacBook-Pro.local>
2022-02-13 18:51:12 -0500

    Fixes to Issue #389

--- src/Components/SignUpPassword.jsx
+++ src/Components/SignUpPassword.jsx
@@ -12,9 +12,16 @@ import {
 import appStyles from './AppStyles';
 import Button from './Button';
 import translate from './getLocalizedText';
+import poorpasswords from './poorpasswords';
 
 export default SignUpPassword = (props) => {
   const [password, setPassword] = useState('');
+  const [passwordStrength, setStrength] = useState();
+  const [charCount, setCount] = useState('');
+  const [lowerCase, setLower] = useState(false);
+  const [upperCase, setUpper] = useState(false);
+  const [number, setNum] = useState(false);
+  const [symbol, setSymbol] = useState(false);
   const [repeat, setRepeat] = useState('');
   const {liveMiami} = props.route.params;
   const {name} = props.route.params;
@@ -34,6 +41,99 @@ export default SignUpPassword = (props) => {
       })
       .done();
   }, []);
+
+  //if user makes changes to password, save password, check password strength
+  let userPassword = (password) => {
+    setPassword(password);
+    checkStrength(password);
+  };
+
+  //check user's password
+  let checkStrength = (password) => {
+   setSymbol(false);
+   setLower(false);
+   setUpper(false);
+   setNum(false);    
+
+   //initialize charCount to 0
+   let charCount = 0;
+
+   //if password contains a lowercase letter...
+   if(/[a-z]/.test(password))
+   {
+	charCount++;
+	setLower(true);
+   }
+   //if password contains an uppercase letter...
+   if(/[A-Z]/.test(password))
+   {
+	charCount++;
+	setUpper(true);
+   }
+   //if password contains a number...
+   if(/[0-9]/.test(password))
+   {
+	charCount++;
+	setNum(true);
+   }
+   //if password contains a symbol...
+   if(/[^A-Za-z0-9]/.test(password))
+   {
+	charCount++;
+	setSymbol(true);
+   }
+
+   setCount(charCount);
+   //if password length is less than 4 and is included in list of poorpasswords...
+   if(password.length <= 4 && poorpasswords.includes(password))
+   {
+	setStrength(0); 		//poor password
+   }
+   //if the password is equal to 5 and has three of the four types...
+   else if(password.length >= 5 && charCount == 3)
+   {
+	setStrength(1);                 //medium password
+   }
+   //if the password is greater than 5 and has all four types...
+   else if(password.length >= 5 && charCount == 4)
+   {
+	setStrength(2);			//strong password
+   }
+   else
+   {
+	setStrength(0); 		//for any other varitations not within core criteria
+   }
+  }
+  
+  let psswordStyle = (passwordStrength) => {
+    let colors;
+    let message = "";
+    let warning = "";
+
+    if(passwordStrength == 0)
+    {
+	colors = appStyles.pinkColor;
+	message = "Password Strength: Poor...";
+	warning = "Password must contain more than 5 characters: including at least 3 of the following: ";
+	warning += "\n\tA number, symbol, an uppercase, or lowercase letter."; 	
+    }
+    if(passWordStrength == 1)
+    {
+	colors = appStyles.blueColor;
+	message = "Password Strength: Medium...";
+    }
+    else
+    {
+	colors = '#298000';
+	message = "Password Strength: High";
+    }
+    return (
+	<View>
+	  <Text style={{color: colors, textAlign: "center"}}>{message}</Text>
+	  <Text style={{color: colors, textAlign: "center"}}>{warning}</Text>
+	</View>
+     );
+  };
 
   let onPress = () => {
     if (password !== repeat) {
