572e75bf90e4b52636d4f4dd761f78ad79d94b47
Julian Lopez <julianlopez6850@gmail.com>
2022-03-11 02:40:51 -0500

    Fix Welcome Banner message
    Welcome Banner message now displays properly as soon as fullName is received from the database.

--- src/Components/WelcomeUserBanner.jsx
+++ src/Components/WelcomeUserBanner.jsx
@@ -1,16 +1,21 @@
 import {Text, TouchableHighlight, View, Alert} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import appStyles from './AppStyles';
 import {getUid, getUserInfo} from '../Firebase';
 import translate from './getLocalizedText';
 
 export default WelcomeUserBanner = (props) => {
+  const [text, setText] = useState();
+
   let fullName = null;
   getUserInfo(getUid()).once('value', (snapshot) => {
     fullName = snapshot.val()?.fullName;
   }); // Get fullName from DB
-  const initialText = `${translate('welcomeUserBanner')}${fullName ?`, ${fullName.split(' ')[0]}!` : ''}`;
-  const [text, setText] = useState(initialText);
+  const getText = () => {setText( `${translate('welcomeUserBanner')}${fullName ? `, ${fullName.split(' ')[0]}!` : ''}`);};
+
+  useEffect(() => {
+    getText();
+  }, [fullName]);
 
   return (
     <TouchableHighlight
