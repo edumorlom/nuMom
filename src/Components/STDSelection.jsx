77132540383786b9307d259fa40df35a98ce6acf
Kimberly Metelus <kimberly@Kimberlys-MBP.attlocal.net>
2022-06-26 15:16:07 -0500

    Fixes to Issue #481

--- src/Components/STDSelection.jsx
+++ src/Components/STDSelection.jsx
@@ -12,36 +12,38 @@ import STD from './STD';
 import translate from './getLocalizedText';
 
 export default function STDSelection(props) {
-  const [data, setData] = useState('');
-  const [sort, setSort] = useState('');
-  const onPress = (std) => {
-    props.navigation.navigate('STDInfo', {
-      name: translate(std.name),
-      symptoms: std.symptoms,
-      diagnosis: std.diagnosis,
-      treatment: std.treatment,
-      consequences: std.consequences,
-      safeSex: std.safeSex,
-    });
+  //  const [data, setData] = useState('');
+  //  const [sort, setSort] = useState('');
+  //  const onPress = (std) => {
+  props.navigation.navigate('STDInfo', {
+    name: translate(std.name),
+    symptoms: std.symptoms,
+    diagnosis: std.diagnosis,
+    treatment: std.treatment,
+    consequences: std.consequences,
+    safeSex: std.safeSex,
+  });
 
-    useEffect(() => {
-      const sortList = () => {
-        const sorts = std.sort((a, b) => a - b);
-        setData[sorts];
-      };
-      sortList(sort);
-    }, [sort]);
+  //    useEffect(() => {
+  //      const sortList = () => {
+  //        const sorts = std.sort((a, b) => a - b);
+  //        setData[sorts];
+  //      };
+  //      sortList(sort);
+  //    }, [sort]);
 
-    return (
-      <ScrollView
-        onChnage={setSort}
-        contentContainerStyle={{
-          alignItems: 'center',
-          maxWidth: '100%',
-          backgroundColor: 'white',
-        }}
-      >
-        {STD().map((std, key) => (
+  return (
+    <ScrollView
+      //        onChnage={setSort}
+      contentContainerStyle={{
+        alignItems: 'center',
+        maxWidth: '100%',
+        backgroundColor: 'white',
+      }}
+    >
+      {STD()
+        .sort()
+        .map((std, key) => (
           <SelectionButton
             style={appStyles.STDFemaleCondomSelectionButton}
             key={key}
@@ -49,7 +51,6 @@ export default function STDSelection(props) {
             onPress={() => onPress(std)}
           />
         ))}
-      </ScrollView>
-    );
-  };
+    </ScrollView>
+  );
 }
