6565947d07679a1a1bffd39b47e5399a22e673d8
aqibshah96 <105533928+aqibshah96@users.noreply.github.com>
2022-11-06 16:29:12 -0500

    Fixes issue # 414
    Minor changes to text along with small formatting changes and some grammar fixes in female condom section. Did not see a reason to resize the pictures as stated in the issue as they seem fine and resizing them caused the text to look less neatly formatted.

---
+++ FemaleCondom.jsx
@@ -0,0 +1,169 @@
+import React, {useState, useEffect} from 'react';
+import {TouchableHighlight, Text, View, ScrollView, Image} from 'react-native';
+import translate from './getLocalizedText';
+import appStyles from './AppStyles';
+import SelectionButton from './SelectionButton';
+import BetterMenu from './BetterMenu';
+import FemaleCondomImg from '../../assets/FemaleCondom.png';
+
+/* Serves to gather all the text for the Do's and Don'ts section
+ * @return array list of condom do's and don'ts
+ */
+function femaleCondomDoList() {
+  return [
+    {
+      do: 'FemaleCondomDo1',
+    },
+    {
+      do: 'FemaleCondomDo2',
+    },
+    {
+      do: 'FemaleCondomDo3',
+    },
+    {
+      do: 'FemaleCondomDo4',
+    },
+    {
+      do: 'FemaleCondomDo5',
+    },
+  ];
+}
+function femaleCondomDontList() {
+  return [
+    {
+      dont: 'FemaleCondomDont1',
+    },
+    {
+      dont: 'FemaleCondomDont2',
+    },
+    {
+      dont: 'FemaleCondomDont3',
+    },
+  ];
+}
+
+/* Displays the taken list from femaleCondomDoDontList
+ * @return formatted View of content from list
+ */
+export function FemaleCondomDoDont() {
+  return (
+    <View style={appStyles.contentContainer}>
+      <ScrollView>
+        {femaleCondomDoList().map((fcdd, key) => (
+          <BetterMenu
+            style={appStyles.FemaleCondomDoMenu}
+            key={key}
+            text={translate(fcdd.do)}
+          />
+        ))}
+        {femaleCondomDontList().map((fcdd, key) => (
+          <BetterMenu
+            style={appStyles.FemaleCondomDontMenu}
+            key={key}
+            text={translate(fcdd.dont)}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+/* Serves to gather all the steps for the How to Insert and Remove a Female Condom section
+ * @return array list of female condom procedure
+ */
+function femaleCondomStepsList() {
+  return [
+    {
+      step: 'FemaleCondomStep1',
+      icon: require('../../assets/FCStep1.jpg'),
+    },
+    {
+      step: 'FemaleCondomStep2',
+      icon: require('../../assets/FCStep2.jpg'),
+    },
+    {
+      step: 'FemaleCondomStep3',
+      icon: require('../../assets/FCStep3.jpg'),
+    },
+    {
+      step: 'FemaleCondomStep4',
+      icon: require('../../assets/FCStep4.jpg'),
+    },
+    {
+      step: 'FemaleCondomStep5',
+      icon: require('../../assets/FCStep5.jpg'),
+    },
+    {
+      step: 'FemaleCondomStep6',
+      icon: require('../../assets/FCStep6.jpg'),
+    },
+    {
+      step: 'FemaleCondomStep7',
+      icon: require('../../assets/FCStep7.jpg'),
+    },
+    {
+      step: 'FemaleCondomStep8',
+      icon: require('../../assets/FCStep8.jpg'),
+    },
+  ];
+}
+
+/* Serves to display all the steps in order and their respective images
+ * @return formatted view of content from femaleCondomStepsList
+ */
+export function FemaleCondomSteps() {
+  return (
+    <View style={appStyles.contentContainer}>
+      <ScrollView>
+        {femaleCondomStepsList().map((fcs, key) => (
+          <BetterMenu
+            style={appStyles.FemaleCondomMenuImage}
+            key={key}
+            text={translate(fcs.step)}
+            icon={fcs.icon}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+/* Main Screen selection for Female Condom, which shows the options for Do's and Dont's, and Insertion and removal etc.
+ * @return formatted view of the FemaleCondomSteps and FemaleCondomDoDont
+ */
+export function FemaleCondomMainScreen(props) {
+  return (
+    <ScrollView contentContainerStyle={appStyles.contentContainer}>
+      <Image
+        style={{justifyContent: 'center', width: 300, height: 140}}
+        source={FemaleCondomImg}
+      />
+      <Text
+        style={{
+          fontSize: appStyles.regularFontSize - 5,
+          justifyContent: 'center',
+          paddingHorizontal: 20,
+          paddingBottom: 5,
+          paddingTop: 5,
+        }}
+      >
+        {translate('WhatIsFemaleCondom')}
+      </Text>
+
+      <SelectionButton
+        style={appStyles.STDFemaleCondomSelectionButton}
+        text={translate('FemaleCondomDoDont')}
+        onPress={() => {
+          props.navigation.navigate('FemaleCondomDoDont');
+        }}
+      />
+      <SelectionButton
+        style={appStyles.STDFemaleCondomSelectionButton}
+        text={translate('FemaleCondomHowTo')}
+        onPress={() => {
+          props.navigation.navigate('FemaleCondomSteps');
+        }}
+      />
+    </ScrollView>
+  );
+}
---
+++ FindCare.jsx
@@ -0,0 +1,150 @@
+import React, {useState, useEffect} from 'react';
+import {ScrollView, View, StyleSheet} from 'react-native';
+import {Dropdown} from 'react-native-material-dropdown-v2';
+import SelectionButton from './SelectionButton';
+import clinicLogo from '../../assets/clinic-logo.png';
+import appStyles, {borderRadius, greyColor, shadow} from './AppStyles';
+import translate from './getLocalizedText';
+
+export default function FindCare(props) {
+  const [dist, setDist] = useState(props.filters[0]);
+  const [service, setService] = useState(props.filters[1]);
+
+  useEffect(() => {
+    // This runs on every re-render
+    setDist(props.filters[0]);
+    setService(props.filters[1]);
+  });
+
+  // props.filterToShow
+
+  const getResourceName = (name) =>
+    name.length > 40 ? `${name.substring(0, 40)}...` : name;
+
+  const window = appStyles.win;
+
+  const clinicsButtons = props.clinics.map((clinic, key) => (
+    <SelectionButton
+      style={appStyles.ClinicSelectionButton}
+      key={key}
+      text={getResourceName(clinic.resource)}
+      subtext={`${clinic.address.street}\n${clinic.address.city}\n${clinic.address.state}, ${clinic.address.zipCode}\n${clinic.distance} miles`}
+      icon={{uri: clinic.logoImage}}
+      onPress={() => {
+        props.setClinicToView(clinic);
+        props.setLowerPanelContent('clinicInfo');
+      }}
+      /* clinic={clinic} */
+    />
+  ));
+
+  let clinics = props.sortedClinics;
+
+  // Filters clinics based on the two filters passed
+  const filterClinics = (distance, service) => {
+    if (distance !== 10000 && clinics) {
+      // if distance filter is not default value
+      clinics = clinics.filter(
+        (clinic) =>
+          // filter by distance
+          clinic.distance <= distance
+      );
+    }
+
+    if (service !== 'All' && clinics) {
+      // if service filter is not default value
+      clinics = clinics.filter((clinic) => clinic.services.includes(service)); // filter by service
+    }
+    // Update the states with the new info
+    props.setClinics(clinics);
+    props.setFilters([distance, service]);
+    setDist(distance);
+    setService(service);
+  };
+
+  const servicesArray = [
+    'All',
+    'Abortions',
+    'Free Materials',
+    'Immunization',
+    'Lab services',
+    'Medical Care',
+    'Pregnancy Tests',
+    'Referrals',
+    'Yearly Exam',
+  ];
+
+  const services = servicesArray.map((service) => ({
+    label: translate(service),
+    value: service,
+  })); // Creates an array of dropdown data
+
+  const distances = [
+    {label: translate('All'), value: 10000},
+    {label: `5 ${translate('Miles')}`, value: 5.5},
+    {label: `10 ${translate('Miles')}`, value: 10.5},
+    {label: `15 ${translate('Miles')}`, value: 15.5},
+    {label: `20 ${translate('Miles')}`, value: 20.5},
+  ];
+
+  return (
+    // The <> tag is shorthand for React.Fragment <= look it up
+    <>
+      {props.filterToShow && ( // If filter set to show, display the filter dropdown
+        <View style={{flexDirection: 'row', height: window.height * 0.085}}>
+          <Dropdown
+            containerStyle={{...styles.Dropdown, right: '10%'}}
+            dropdownOffset={{top: 0, bottom: 0, left: 0}}
+            pickerStyle={styles.Picker}
+            inputContainerStyle={{borderBottomColor: 'transparent'}}
+            textAlign="center"
+            itemTextStyle={{alignSelf: 'center'}}
+            fontSize={12}
+            data={distances}
+            label={translate('Distance')}
+            value={dist}
+            useNativeDriver
+            onChangeText={(value, index, data) => filterClinics(value, service)}
+          />
+
+          <Dropdown
+            containerStyle={{...styles.Dropdown, left: '10%'}}
+            dropdownOffset={{top: 0, bottom: 0, left: 0}}
+            pickerStyle={styles.Picker}
+            inputContainerStyle={{borderBottomColor: 'transparent'}}
+            textAlign="center"
+            itemTextStyle={{alignSelf: 'center'}}
+            fontSize={12}
+            data={services}
+            label={translate('services')}
+            value={service}
+            useNativeDriver
+            onChangeText={(value, index, data) => filterClinics(dist, value)}
+          />
+        </View>
+      )}
+      <ScrollView
+        contentContainerStyle={{alignItems: 'center', maxWidth: '100%'}}
+      >
+        {clinicsButtons}
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  Dropdown: {
+    backgroundColor: 'white',
+    ...shadow,
+    borderColor: greyColor,
+    borderRadius: 5,
+    width: '42%',
+    bottom: 5,
+  },
+  Picker: {
+    backgroundColor: 'white',
+    ...shadow,
+    borderColor: greyColor,
+    borderRadius: 15,
+  },
+});
