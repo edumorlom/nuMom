31b2a979c4595a2564eebf121dceba4eb826fe0a
JoseBaez010 <josebaez0968@gmail.com>
2023-10-30 15:58:43 -0400

    Fixed scrolling problem

--- src/Components/LowerPanel.jsx
+++ src/Components/LowerPanel.jsx
@@ -122,8 +122,8 @@ export default LowerPanel = (props) => {
         ...appStyles.lowerPanel,
         ...(mapToggle && moveAnim ? { top: moveAnim } : {}),
         overflow: 'hidden',
-        height: isFullScreen ? '100%' : '90%',
-        paddingTop: 40, // Added padding so back button would not get cut off when viewing facilites page
+        height: isFullScreen ? '100%' : '65%',
+        paddingTop: 20, // Added padding so back button would not get cut off when viewing facilites page
       }}
     >
       {props.lowerPanelContent !== 'selection' && (
